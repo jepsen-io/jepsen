687cda83b0e7aef987ca1217a3bb563ab2e87e86
aphyr <aphyr@aphyr.com>
2013-04-14 13:33:43 -0700

    Managed to create split-brain redis with sentinel.

--- salticid/main.rb
+++ salticid/main.rb
@@ -110,7 +110,51 @@ end
 role :redis do
   task :setup do
     sudo do
-      exec! 'apt-get install -y redis-server'
+      cd '/opt/'
+      unless dir? :redis
+        git :clone, 'git://github.com/antirez/redis.git', echo: true
+      end
+      cd :redis
+      make echo: true
+#      make :test, echo: true
+    end
+  end
+
+  task :start do
+    cd '/opt/redis/src'
+    sudo do
+      if name == 'n1'
+        # master
+        exec! 'bash -c "ulimit -n 10000 && ./redis-server"', echo: true
+      else
+        exec! 'bash -c "ulimit -n 10000 && ./redis-server --slaveof n1 6379"', echo: true
+      end
+    end
+  end
+
+  task :sentinel do
+    sudo do
+      echo "port 26379
+sentinel monitor mymaster n1 6379 2
+sentinel down-after-milliseconds mymaster 2000
+sentinel failover-timeout mymaster 900000
+sentinel can-failover mymaster yes
+sentinel parallel-syncs mymaster 1", to: '/opt/redis/sentinel.config'
+      cd '/opt/redis/src'
+      exec! './redis-sentinel /opt/redis/sentinel.config', echo: true
+    end
+  end
+
+  task :stop do
+    sudo do
+      killall 'redis-server' rescue log "no redis-server"
+      killall 'redis-sentinel' rescue log "no redis-sentinel"
+    end
+  end
+
+  task :replication do
+    sudo do
+      exec! '/opt/redis/src/redis-cli info replication', echo: true
     end
   end
 end
