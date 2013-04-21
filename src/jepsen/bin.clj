4a75e10372ccaa1c2617e4dbd14faef086269b63
aphyr <aphyr@aphyr.com>
2013-04-21 16:20:36 -0700

    lein run can run a bunch of sample apps.

--- src/jepsen/bin.clj
+++ src/jepsen/bin.clj
@@ -1,9 +1,30 @@
 (ns jepsen.bin
+  (:require clojure.stacktrace)
   (:use jepsen.set-app
         jepsen.riak
         jepsen.mongo
         jepsen.redis))
 
-(defn -main []
-  (run (apps mongo-app))
-  (System/exit 0))
+(def app-map
+  "A map from command-line names to apps you can run"
+  {"mongo-replicas-safe"    mongo-replicas-safe-app
+   "mongo-safe"             mongo-safe-app
+   "mongo-unsafe"           mongo-unsafe-app
+   "mongo"                  mongo-app
+   "redis"                  redis-app
+   "riak-lww-all"           riak-lww-all-app
+   "riak-lww-quorum"        riak-lww-quorum-app
+   "riak-lww-sloppy-quorum" riak-lww-sloppy-quorum-app
+   "riak-crdt"              riak-crdt-app})
+
+(defn -main
+  ([]
+   (println "Available apps:")
+   (dorun (map println (keys app-map))))
+  ([app]
+   (try
+     (run (apps (app-map app)))
+     (System/exit 0)
+     (catch Throwable t
+       (clojure.stacktrace/print-cause-trace t)
+       (System/exit 1)))))
