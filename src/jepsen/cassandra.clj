e9ab64703423f7b3af24c4dee32501c37077375f
aphyr <aphyr@aphyr.com>
2013-08-31 22:50:07 -0700

    More cassandra tests

--- src/jepsen/cassandra.clj
+++ src/jepsen/cassandra.clj
@@ -3,7 +3,9 @@
   (:import (com.datastax.driver.core ConsistencyLevel))
   (:require [clojurewerkz.cassaforte.client :as client]
             [clojurewerkz.cassaforte.multi.cql :as cql]
-            [jepsen.codec           :as codec])
+            qbits.hayt.cql
+            [jepsen.codec           :as codec]
+            [clojure.set            :as set])
   (:use
     clojurewerkz.cassaforte.query
     jepsen.util
@@ -90,7 +92,7 @@
                                                :primary-key [:id]})))
       
       (add [app element]
-        (client/with-consistency-level ConsistencyLevel/QUORUM
+        (client/with-consistency-level ConsistencyLevel/ONE
           (cql/update session table
                       {:count (increment-by 1)}
                       (where :id 0)))
@@ -105,3 +107,100 @@
 
       (teardown [app]
         (drop-keyspace session)))))
+
+(defn set-app
+  [opts]
+  (let [table "set_app"
+        cluster (client/build-cluster {:contact-points [(:host opts)]
+                                       :port 9042})
+        session (client/connect cluster)]
+    (reify SetApp
+      (setup [app]
+        (teardown app)
+        (ensure-keyspace session)
+        (cql/create-table session table
+                          (column-definitions {:id :int
+                                               :elements (set-type :int)
+                                               :primary-key [:id]}))
+        (cql/insert session table
+                    {:id 0
+                     :elements #{}}))
+
+      (add [app element]
+        (client/with-consistency-level ConsistencyLevel/QUORUM
+          (cql/update session table
+                      {:elements [+ #{element}]}
+                      (where :id 0)))
+        ok)
+      
+      (results [app]
+        (client/with-consistency-level ConsistencyLevel/ALL
+          (->> (cql/select session table (where :id 0))
+               first
+               :elements)))
+
+      (teardown [app]
+        (drop-keyspace session)))))
+
+(defn isolation-app
+  "This app tests whether or not it is possible to consistently update multiple
+  cells in a row, such that either *both* writes are visible together, or
+  *neither* is.
+
+  Each client picks a random int identifier to distinguish itself from the
+  other clients. It tries to write this identifier to cell A, and -identifier
+  to cell B. The write is considered successful if A=-B. It is unsuccessful if
+  A is *not* equal to -B; e.g. our updates were not isolated.
+  
+'concurrency defines the number of writes made to each row. "
+  [opts]
+  (let [table       "isolation_app"
+        concurrency 5
+        client-id   (rand-int Integer/MAX_VALUE)
+        cluster     (client/build-cluster {:contact-points [(:host opts)]
+                                           :port 9042})
+        session     (client/connect cluster)]
+    (reify SetApp
+      (setup [app]
+        (teardown app)
+        (ensure-keyspace session)
+        (cql/create-table session table
+                          (column-definitions {:id :int
+                                               :a  :int
+                                               :b  :int
+                                               :primary-key [:id]})))
+
+      (add [app element]
+        (client/with-consistency-level ConsistencyLevel/ANY
+          (dotimes [i concurrency]
+            (let [e (- element i)]
+              (when (<= 0 e)
+                (client/execute
+                  session
+                  (->> (insert-query table
+                                     {:id e
+                                      :a client-id
+                                      :b (- client-id)})
+                       ; If you force timestamp collisions instead of letting
+                       ; them happen naturally, you can reliably cause
+                       ; conflicts in 99% of rows! :D
+                       ; (using :timestamp 1)
+                       queries
+                       batch-query
+                       client/render-query)
+                  :prepared qbits.hayt.cql/*prepared-statement*)))))
+        ok)
+
+      (results [app]
+        (client/with-consistency-level ConsistencyLevel/ALL
+          (->> (cql/select session table)
+               (remove #(= (:a %) (- (:b %))))
+               prn
+               dorun)
+
+          (->> (cql/select session table)
+               (filter #(= (:a %) (- (:b %))))
+               (map :id))))
+
+      (teardown [app]
+        (drop-keyspace session))))) 
