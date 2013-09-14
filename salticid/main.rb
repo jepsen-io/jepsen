3a081f446f20a04094e815c35301011f7984bbd1
Brett Hoerner <brett@bretthoerner.com>
2013-09-14 15:48:11 -0500

    Use getent instead of dig so that /etc/hosts is respected.

--- salticid/main.rb
+++ salticid/main.rb
@@ -45,9 +45,9 @@ role :jepsen do
 
   task :partition do
     sudo do
-      n3 = dig '+short', :n3
-      n4 = dig '+short', :n4
-      n5 = dig '+short', :n5
+      n3 = (getent 'ahosts', :n3).split("\n").first.split(" ").first
+      n4 = (getent 'ahosts', :n4).split("\n").first.split(" ").first
+      n5 = (getent 'ahosts', :n5).split("\n").first.split(" ").first
       if ['n1', 'n2'].include? name
         log "Partitioning from n3, n4 and n5."
         iptables '-A', 'INPUT', '-s', n3, '-j', 'DROP'
@@ -60,11 +60,11 @@ role :jepsen do
 
   task :partition_reject do
     sudo do
-      n1 = dig '+short', :n1
-      n2 = dig '+short', :n2
-      n3 = dig '+short', :n3
-      n4 = dig '+short', :n4
-      n5 = dig '+short', :n5
+      n1 = (getent 'ahosts', :n1).split("\n").first.split(" ").first
+      n2 = (getent 'ahosts', :n2).split("\n").first.split(" ").first
+      n3 = (getent 'ahosts', :n3).split("\n").first.split(" ").first
+      n4 = (getent 'ahosts', :n4).split("\n").first.split(" ").first
+      n5 = (getent 'ahosts', :n5).split("\n").first.split(" ").first
       if ['n1', 'n2'].include? name
         log "Partitioning from n3, n4 and n5."
         iptables '-A', 'INPUT', '-s', n3, '-j', 'REJECT'
