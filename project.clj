65f69bf5fb03015bfe623023c6e8b33d91730944
aphyr <aphyr@aphyr.com>
2014-01-16 15:33:56 -0800

    0.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jepsen "0.0.1-SNAPSHOT"
+(defproject jepsen "0.0.1"
             :description "Call Me Maybe: Network Partitions in Practice"
             :dependencies [[org.clojure/clojure "1.5.1"]
                            [aleph "0.3.0-beta16"]
