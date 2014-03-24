3b3d02b0ab9d7c8c8d7908a0b67d4c42b4ceae02
Jennifer Rullmann <jen@redmonocle.com>
2014-03-24 10:45:22 -0400

    Updated knossos dependency to current version in Clojars.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
             :description "Call Me Maybe: Network Partitions in Practice"
             :dependencies [[org.clojure/clojure "1.6.0-beta1"]
                            [aleph "0.3.0-beta16"]
-                           [knossos "0.1.1-SNAPSHOT"]
+                           [knossos "0.1.0"]
                            [clj-ssh "0.5.7"]
                            [com.novemberain/welle "1.6.0-beta1"]
                            [com.taoensso/carmine "1.6.0"]
