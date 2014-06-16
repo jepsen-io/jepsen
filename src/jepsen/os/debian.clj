c6cf715d26a36b9543d338548de4baa720e65c69
Aphyr <aphyr@aphyr.com>
2014-06-15 23:31:14 -0700

    os/debian: fix installed? with keyword args

--- src/jepsen/os/debian.clj
+++ src/jepsen/os/debian.clj
@@ -25,8 +25,8 @@
 
 
 (defn installed
-  "Given a list of debian packages, returns the set of packages which are
-  installed."
+  "Given a list of debian packages (strings, symbols, keywords, etc), returns
+  the set of packages which are installed, as strings."
   [pkgs]
   (let [pkgs (->> pkgs (map name) set)]
     (->> (apply c/exec :dpkg :--get-selections pkgs)
@@ -41,7 +41,7 @@
   system?"
   [pkg-or-pkgs]
   (let [pkgs (if (coll? pkg-or-pkgs) pkg-or-pkgs (list pkg-or-pkgs))]
-    (= (set pkgs) (installed pkgs))))
+    (every? (installed pkgs) (map name pkgs))))
 
 (defn install
   "Ensure the given packages are installed."
