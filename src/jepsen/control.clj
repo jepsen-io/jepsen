1527df8a4d948665c8e22289295bfc24230da0c4
Aphyr <aphyr@aphyr.com>
2015-02-11 12:13:00 -0800

    Default credentials to root:root

--- src/jepsen/control.clj
+++ src/jepsen/control.clj
@@ -12,8 +12,8 @@
 (def ^:dynamic *trace*    "Shall we trace commands?"      false)
 (def ^:dynamic *dir*      "Working directory"             "/")
 (def ^:dynamic *sudo*     "User to sudo to"               nil)
-(def ^:dynamic *username* "Username"                      "ubuntu")
-(def ^:dynamic *password* "Password (for login and sudo)" "ubuntu")
+(def ^:dynamic *username* "Username"                      "root")
+(def ^:dynamic *password* "Password (for login and sudo)" "root")
 (def ^:dynamic *private-key-path*         "SSH identity file"     nil)
 (def ^:dynamic *strict-host-key-checking* "Verify SSH host keys"  :yes)
 
