c4ffee78c7f83710cc8babf983e7cdd8e688707c
Aphyr <aphyr@aphyr.com>
2015-04-01 09:39:30 -0700

    move elasticsearch to new project

---
+++ elasticsearch/project.clj
@@ -0,0 +1,8 @@
+(defproject elasticsearch "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [jepsen "0.0.3-SNAPSHOT"]
+                 [elastisch "2.2.0-beta2"]])
