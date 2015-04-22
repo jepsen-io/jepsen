582c5ec6a2f73b7ac650d26a255c605183d71486
Jonathan Halterman <jhalterman@gmail.com>
2015-04-22 15:42:06 -0700

    Update Aerospike, Elasticsearch, and MongoDB jepsen dependencies to 0.0.3

--- aerospike/project.clj
+++ aerospike/project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [jepsen "0.0.3-SNAPSHOT"]
+                 [jepsen "0.0.3"]
                  [com.aerospike/aerospike-client "3.1.0"]])
