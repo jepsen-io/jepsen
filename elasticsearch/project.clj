8be9e38f28d659b822a9f56667b92ab251cba250
Aphyr <aphyr@aphyr.com>
2015-04-27 15:33:14 -0700

    Bump deps to jepsen 0.0.3

--- elasticsearch/project.clj
+++ elasticsearch/project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "1.1.0"]
-                 [jepsen "0.0.3-SNAPSHOT"]
+                 [jepsen "0.0.3"]
                  [clojurewerkz/elastisch "2.2.0-beta2"]])
