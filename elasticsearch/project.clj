4fcd99bdfc3e922a5360005f484af155c03ef762
Aphyr <aphyr@aphyr.com>
2015-04-27 23:02:31 -0700

    elasticsearch version bump

--- elasticsearch/project.clj
+++ elasticsearch/project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "1.1.0"]
-                 [jepsen "0.0.3"]
+                 [jepsen "0.0.4-SNAPSHOT"]
                  [clojurewerkz/elastisch "2.2.0-beta2"]])
