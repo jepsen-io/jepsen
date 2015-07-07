630c8cf0cbb0b032415763f64f670473f7235142
Aphyr <aphyr@aphyr.com>
2015-07-07 11:32:25 -0700

    mongodb 0.1.0

--- mongodb/project.clj
+++ mongodb/project.clj
@@ -1,4 +1,4 @@
-(defproject jepsen.mongodb "0.1.0-SNAPSHOT"
+(defproject jepsen.mongodb "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
