30536c7b4ea95f544facaddccd83a1c3d8da255b
Aphyr <aphyr@aphyr.com>
2015-07-22 13:48:03 -0700

    mongodb-rocks: indexes, 2:1 write/delete ratio

--- mongodb-rocks/test/jepsen/mongodb_rocks_test.clj
+++ mongodb-rocks/test/jepsen/mongodb_rocks_test.clj
@@ -4,6 +4,7 @@
             [jepsen.mongodb-rocks :refer :all]))
 
 (deftest logger-perf
+  (is (:valid? (:results (run! (logger-perf-test "3.0.4~pre" "wiredTiger")))))
   (is (:valid? (:results (run! (logger-perf-test "3.0.4~pre" "mmapv1")))))
   (is (:valid? (:results (run! (logger-perf-test "3.0.4~pre" "rocksdb")))))
   )
