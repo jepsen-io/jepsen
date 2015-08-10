cc392e5a868d686f3b98ff9c148d302ed477fd06
Aphyr <aphyr@aphyr.com>
2015-08-10 16:02:46 -0700

    jepsen 0.0.6, chronos 0.1.0, zk 0.1.0

--- chronos/project.clj
+++ chronos/project.clj
@@ -1,11 +1,11 @@
-(defproject jepsen.chronos "0.1.0-SNAPSHOT"
+(defproject jepsen.chronos "0.1.0"
   :description "Jepsen Mesos/Chronos test"
   :url "https://github.com/aphyr/jepsen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [jepsen.zookeeper "0.1.0-SNAPSHOT"]
-                 [jepsen "0.0.6-SNAPSHOT"]
+                 [jepsen.zookeeper "0.1.0"]
+                 [jepsen "0.0.6"]
                  [cheshire "5.5.0"]
                  [clj-http "2.0.0"]
                  [clj-time "0.10.0"]
--- zookeeper/project.clj
+++ zookeeper/project.clj
@@ -1,8 +1,8 @@
-(defproject jepsen.zookeeper "0.1.0-SNAPSHOT"
+(defproject jepsen.zookeeper "0.1.0"
   :description "Jepsen Zookeeper test"
   :url "https://github.com/aphyr/zookeeper"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [jepsen "0.0.6-SNAPSHOT"]
+                 [jepsen "0.0.6"]
                  [org.apache.curator/curator-recipes "2.0.1-incubating"]])
