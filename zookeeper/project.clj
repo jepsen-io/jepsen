9af656f9fa8713d79dd28c6f09db58acd4cf01cb
Aphyr <aphyr@aphyr.com>
2015-12-31 00:06:23 -0800

    zk: avout test

--- zookeeper/project.clj
+++ zookeeper/project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/aphyr/zookeeper"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [jepsen "0.0.6"]
-                 [org.apache.curator/curator-recipes "2.9.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [jepsen "0.0.8"]
+                 [avout "0.5.4"]])
--- zookeeper/src/jepsen/zookeeper.clj
+++ zookeeper/src/jepsen/zookeeper.clj
@@ -8,9 +8,12 @@
              [control :as c :refer [|]]
              [checker :as checker]
              [generator :as gen]
-             [util :refer [timeout]]]
+             [util :refer [timeout]]
+             [nemesis :as nemesis]]
             [jepsen.control.util :as cu]
-            [jepsen.os.debian :as debian]))
+            [jepsen.os.debian :as debian]
+            [knossos.model :as model]
+            [avout.core :as avout]))
 
 (defn zk-node-ids
   "We number nodes in reverse order so the leader is the first node. Returns a
@@ -69,10 +72,52 @@
     (log-files [_ _ _]
       ["/var/log/zookeeper/zookeeper.log"])))
 
+(defn client
+  [conn a]
+  (reify client/Client
+    (setup! [_ test node]
+      (let [conn (avout/connect (name node))
+            a    (avout/zk-atom conn "/jepsen" 0)]
+        (client conn a)))
+
+    (invoke! [this test op]
+      (timeout 5000 (assoc op :type :info, :error :timeout)
+               (case (:f op)
+                 :read  (assoc op :type :ok, :value @a)
+                 :write (do (avout/reset!! a (:value op))
+                            (assoc op :type :ok))
+                 :cas   (let [[value value'] (:value op)
+                              type           (atom :fail)]
+                          (avout/swap!! a (fn [current]
+                                            (if (= current value)
+                                              (do (reset! type :ok)
+                                                  value')
+                                              (do (reset! type :fail)
+                                                  current))))
+                          (assoc op :type @type)))))
+
+    (teardown! [_ test]
+      (.close conn))))
+
+(defn r [_ _] {:type :invoke, :f :read, :value nil})
+(defn w [_ _] {:type :invoke, :f :write, :value (rand-int 5)})
+(defn cas [_ _] {:type :invoke, :f :cas, :value [(rand-int 5) (rand-int 5)]})
+
 (defn simple-test
   [version]
   (assoc tests/noop-test
          :name "zookeeper"
          :os   debian/os
          :db   (db version)
-         :generator (gen/sleep 30)))
+         :generator (->> (gen/mix [r w cas])
+                         (gen/stagger 1)
+                         (gen/nemesis
+                           (gen/seq (cycle [(gen/sleep 5)
+                                            {:type :info, :f :start}
+                                            (gen/sleep 5)
+                                            {:type :info, :f :stop}])))
+                         (gen/time-limit 15))
+         :nemesis (nemesis/partition-random-halves)
+         :client  (client nil nil)
+         :model   (model/cas-register 0)
+         :checker checker/linearizable))
