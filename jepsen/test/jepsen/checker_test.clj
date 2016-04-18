a651459e12b40a521a4fdfe802e9e8d66d348cb1
Aphyr <aphyr@aphyr.com>
2016-04-18 12:45:48 -0500

    Tests: Update for new checker arity

--- jepsen/test/jepsen/checker_test.clj
+++ jepsen/test/jepsen/checker_test.clj
@@ -9,29 +9,29 @@
 
 (deftest queue-test
   (testing "empty"
-    (is (:valid? (check queue nil nil []))))
+    (is (:valid? (check queue nil nil [] {}))))
 
   (testing "Possible enqueue but no dequeue"
     (is (:valid? (check queue nil (model/unordered-queue)
-                        [(invoke-op 1 :enqueue 1)]))))
+                        [(invoke-op 1 :enqueue 1)] {}))))
 
   (testing "Definite enqueue but no dequeue"
     (is (:valid? (check queue nil (model/unordered-queue)
-                        [(ok-op 1 :enqueue 1)]))))
+                        [(ok-op 1 :enqueue 1)] {}))))
 
   (testing "concurrent enqueue/dequeue"
     (is (:valid? (check queue nil (model/unordered-queue)
                         [(invoke-op 2 :dequeue nil)
                          (invoke-op 1 :enqueue 1)
-                         (ok-op     2 :dequeue 1)]))))
+                         (ok-op     2 :dequeue 1)] {}))))
 
   (testing "dequeue but no enqueue"
     (is (not (:valid? (check queue nil (model/unordered-queue)
-                             [(ok-op 1 :dequeue 1)]))))))
+                             [(ok-op 1 :dequeue 1)] {}))))))
 
 (deftest total-queue-test
   (testing "empty"
-    (is (:valid? (check total-queue nil nil []))))
+    (is (:valid? (check total-queue nil nil [] {}))))
 
   (testing "sane"
     (is (= (check total-queue nil nil
@@ -41,7 +41,8 @@
                    (invoke-op 3 :dequeue 1)
                    (ok-op     3 :dequeue 1)
                    (invoke-op 3 :dequeue 2)
-                   (ok-op     3 :dequeue 2)])
+                   (ok-op     3 :dequeue 2)]
+                  {})
            {:valid?           true
             :duplicated       (multiset/multiset)
             :lost             (multiset/multiset)
@@ -66,7 +67,8 @@
                    (invoke-op 6 :dequeue nil)
                    (ok-op     6 :dequeue :dup)
                    (invoke-op 7 :dequeue nil)
-                   (ok-op     7 :dequeue :dup)])
+                   (ok-op     7 :dequeue :dup)]
+                  {})
            {:valid?           false
             :lost             (multiset/multiset :enqueued)
             :unexpected       #{:wtf}
@@ -80,7 +82,7 @@
 
 (deftest counter-test
   (testing "empty"
-    (is (= (check counter nil nil [])
+    (is (= (check counter nil nil [] {})
            {:valid? true
             :reads  []
             :errors []})))
@@ -88,7 +90,8 @@
   (testing "initial read"
     (is (= (check counter nil nil
                   [(invoke-op 0 :read nil)
-                   (ok-op     0 :read 0)])
+                   (ok-op     0 :read 0)]
+                  {})
            {:valid? true
             :reads  [[0 0 0]]
             :errors []})))
@@ -96,7 +99,8 @@
   (testing "initial invalid read"
     (is (= (check counter nil nil
                   [(invoke-op 0 :read nil)
-                   (ok-op     0 :read 1)])
+                   (ok-op     0 :read 1)]
+                  {})
            {:valid? false
             :reads  [[0 1 0]]
             :errors [[0 1 0]]})))
@@ -120,7 +124,8 @@
                    (ok-op     5 :add 4)
                    (ok-op     6 :read 100)
                    (ok-op     7 :add 8)
-                   (ok-op     8 :read 15)])
+                   (ok-op     8 :read 15)]
+                  {})
            {:valid? false
             :reads  [[0 6 15] [0 0 15] [0 3 15] [0 100 15] [0 15 15]]
             :errors [[0 100 15]]})))
@@ -136,14 +141,14 @@
                    (ok-op     0 :read 3)
                    (invoke-op 0 :read nil)
                    (ok-op     1 :add  2)
-                   (ok-op     0 :read 5)])
+                   (ok-op     0 :read 5)] {})
            {:valid? false
             :reads  [[0 0 1] [0 3 3] [1 5 3]]
             :errors [[1 5 3]]}))))
 
 (deftest compose-test
   (is (= (check (compose {:a unbridled-optimism :b unbridled-optimism})
-                nil nil nil)
+                nil nil nil {})
          {:a {:valid? true}
           :b {:valid? true}
           :valid? true})))
@@ -193,4 +198,5 @@
                           [{:type :invoke, :f f, :time time}
                            {:type type,    :f f, :time (+ time latency)}])))
               (take 10000)
-              vec)))
+              vec)
+         {}))
