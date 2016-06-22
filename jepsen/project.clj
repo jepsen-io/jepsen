a52fc01b7209c8d2f86457c70d65a9cd6ef9f75b
Aphyr <aphyr@aphyr.com>
2016-06-22 09:56:43 -0500

    0.1.1

--- jepsen/project.clj
+++ jepsen/project.clj
@@ -1,10 +1,10 @@
-(defproject jepsen "0.1.1-SNAPSHOT"
+(defproject jepsen "0.1.1"
   :description "Call Me Maybe: Network Partitions in Practice"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.fressian "0.2.1"]
                  [org.clojure/tools.logging "0.3.1"]
                  [clj-time "0.11.0"]
-                 [knossos "0.2.8-SNAPSHOT"]
+                 [knossos "0.2.8"]
                  [clj-ssh "0.5.14"]
                  [gnuplot "0.1.1"]
                  [hiccup "1.0.5"]
