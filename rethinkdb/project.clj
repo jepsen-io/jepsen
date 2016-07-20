ce7eec90fde7dd84c7cf04a05582bb57929e7e70
Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
2016-07-20 02:32:46 +0000

    Fix rethinkdb
    Fix #138

--- rethinkdb/project.clj
+++ rethinkdb/project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.apa512/rethinkdb "0.12.2-SNAPSHOT"]
                  [clj-time "0.9.0"]
-                 [jepsen "0.1.0-SNAPSHOT"]
+                 [jepsen "0.1.2"]
                  [cheshire "5.4.0"]]
   :jvm-opts ["-Xmx64g"
              "-XX:+UseConcMarkSweepGC"
