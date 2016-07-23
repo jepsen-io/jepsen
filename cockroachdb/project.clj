2192c474ffdd5df8b7b02268c46f51fab686495d
Aphyr <aphyr@aphyr.com>
2016-07-22 20:42:43 -0500

    cockroach: register test now costs way less, runs keys concurrently

--- cockroachdb/project.clj
+++ cockroachdb/project.clj
@@ -3,12 +3,12 @@
   :url "http://cockroachlabs.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [jepsen "0.1.2"]
                  [org.clojure/tools.cli "0.3.3"]
                  [org.clojure/java.jdbc "0.4.1"]
                  [org.postgresql/postgresql "9.4.1208"]]
-  :jvm-opts ["-Xmx8g"
+  :jvm-opts ["-Xmx64g"
              "-XX:+UseConcMarkSweepGC"
              "-XX:+UseParNewGC"
              "-XX:+CMSParallelRemarkEnabled"
--- cockroachdb/src/jepsen/cockroach/register.clj
+++ cockroachdb/src/jepsen/cockroach/register.clj
@@ -7,6 +7,7 @@
                     [generator :as gen]
                     [independent :as independent]
                     [util :refer [meh]]]
+            [jepsen.checker.timeline :as timeline]
             [jepsen.cockroach.nemesis :as cln]
             [clojure.java.jdbc :as j]
             [clojure.tools.logging :refer :all]
@@ -16,7 +17,7 @@
 (defn w   [_ _] {:type :invoke, :f :write, :value (rand-int 5)})
 (defn cas [_ _] {:type :invoke, :f :cas, :value [(rand-int 5) (rand-int 5)]})
 
-(defrecord AtomicClient [tbl-created?]
+(defrecord AtomicClient [tbl-created? conn]
   client/Client
 
   (setup! [this test node]
@@ -26,59 +27,62 @@
       (locking tbl-created?
         (when (compare-and-set! tbl-created? false true)
           (Thread/sleep 1000)
-          (c/with-txn-notimeout {} [c conn] (j/execute! c ["drop table if exists test"]))
+          (c/with-txn-notimeout {} [c conn]
+            (j/execute! c ["drop table if exists test"]))
           (Thread/sleep 1000)
           (info node "Creating table")
-          (c/with-txn-notimeout {} [c conn] (j/execute! c ["create table test (id int, val int)"]))))
+          (c/with-txn-notimeout {} [c conn]
+            (j/execute! c ["create table test (id int, val int)"]))))
 
       (assoc this :conn conn)))
 
   (invoke! [this test op]
-    (let [conn (:conn this)]
+    (c/with-idempotent #{:read}
       (c/with-txn op [c conn]
         (let [id     (key (:value op))
-              value  (val (:value op))
-              val'    (->> (j/query c ["select val from test where id = ?" id] :row-fn :val)
-                           (first))]
+              val'   (val (:value op))
+              val    (first (j/query c ["select val from test where id = ?" id]
+                                     :row-fn :val))]
           (case (:f op)
-            :read (assoc op :type :ok, :value (independent/tuple id val'))
+            :read (assoc op :type :ok, :value (independent/tuple id val))
 
             :write (do
-                     (if (nil? val')
-                       (j/insert! c :test {:id id :val value})
-                       (j/update! c :test {:val value} ["id = ?" id]))
+                     (if (nil? val)
+                       (j/insert! c :test {:id id :val val'})
+                       (j/update! c :test {:val val'} ["id = ?" id]))
                      (assoc op :type :ok))
 
-            :cas (let [[value' value] value
-                       cnt (j/update! c :test {:val value} ["id = ? and val = ?" id value'])]
-                   (assoc op :type (if (zero? (first cnt)) :fail :ok))))
-          ))))
+            :cas (let [[expected-val new-val] val'
+                       cnt (j/update! c :test {:val new-val}
+                                      ["id = ? and val = ?" id expected-val])]
+                   (assoc op :type (if (zero? (first cnt)) :fail :ok))))))))
 
   (teardown! [this test]
-    (let [conn (:conn this)]
-      (meh (c/with-timeout conn nil
-             (j/execute! @conn ["drop table test"])))
-      (c/close-conn @conn))
-    ))
+    (meh (c/with-timeout conn nil
+           (j/execute! @conn ["drop table test"])))
+    (c/close-conn @conn)))
 
 (defn test
   [opts]
   (c/basic-test
     (merge
-      {:name    "atomic"
+      {:name        "atomic"
        :concurrency c/concurrency-factor
-       :client  (AtomicClient. (atom false))
-       :generator (->> (independent/sequential-generator
-                         (range)
-                         (fn [k]
-                           (->> (gen/reserve 5 (gen/mix [w cas]) r)
-                                (gen/delay 0.5)
-                                (gen/limit 60))))
-                       (gen/stagger 1)
-                       (cln/with-nemesis (:generator (:nemesis opts))))
+       :client      (AtomicClient. (atom false) nil)
+       :generator   (->> (independent/concurrent-generator 10
+                           (range)
+                           (fn [k]
+                             (->> (gen/reserve 5 (gen/mix [w cas]) r)
+                                  (gen/delay 0.5)
+                                  (gen/limit 60))))
+                         (gen/stagger 1)
+                         (cln/with-nemesis (:generator (:nemesis opts))))
 
-       :model   (model/cas-register 0)
-       :checker (checker/compose
-                  {:perf   (checker/perf)
-                   :details (independent/checker checker/linearizable) })}
+       :model       (model/cas-register 0)
+       :checker     (checker/compose
+                      {:perf   (checker/perf)
+                       :details (independent/checker
+                                  (checker/compose
+                                    {:timeline     (timeline/html)
+                                     :linearizable checker/linearizable}))})}
       opts)))
