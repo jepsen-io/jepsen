0f725055f73a9b7c1bcf5e2cd4815b63ceb10979
Aphyr <aphyr@aphyr.com>
2016-11-08 15:38:28 -0600

    Cockroach: 0.1.0, depend on jepsen 0.1.3

--- cockroachdb/project.clj
+++ cockroachdb/project.clj
@@ -1,10 +1,10 @@
-(defproject cockroachdb "0.1.0-SNAPSHOT"
+(defproject cockroachdb "0.1.0"
   :description "Jepsen testing for CockroachDB"
   :url "http://cockroachlabs.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [jepsen "0.1.3-SNAPSHOT"]
+                 [jepsen "0.1.3"]
                  [org.clojure/java.jdbc "0.6.1"]
                  [circleci/clj-yaml "0.5.5"]
                  [org.postgresql/postgresql "9.4.1211"]]
