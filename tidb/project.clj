14018f46d5a8e40040b2c9345dc05b8e7ee86fea
UncP <uncp.xu@gmail.com>
2017-07-25 16:22:37 +0800

    client module for TiJepsen
    Signed-off-by: UncP <uncp.xu@gmail.com>

--- tidb/project.clj
+++ tidb/project.clj
@@ -6,5 +6,7 @@
   :main tidb.core
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [jepsen "0.1.5"]
+                 [org.clojure/java.jdbc "0.7.0"]
+                 [mysql/mysql-connector-java "5.1.20"]
                 ]
 )
--- tidb/src/tidb/core.clj
+++ tidb/src/tidb/core.clj
@@ -7,18 +7,32 @@
             ]
             [jepsen.os.debian :as debian]
             [tidb.db :as db]
+            [tidb.client :as client]
   )
 )
 
-(defn tidb-test
+(def n 2)
+(def initial-balance 10)
+
+(defn bank-test
   [opts]
-    (merge tests/noop-test
-      {:name "TiDB"
-       :os debian/os
-       :db (db/db opts)
-      }
-      opts
-    )
+  (merge tests/noop-test
+    {:os debian/os
+     :name "TiDB-Bank"
+     :concurrency 20
+     :model  {:n n :total (* n initial-balance)}
+     :db (db/db opts)
+     :client (client/bank-client n initial-balance " FOR UPDATE" false)
+     :generator (gen/phases
+                  (->> (gen/mix [client/bank-read client/bank-diff-transfer])
+                       (gen/clients)
+                       (gen/stagger 1/10)
+                       (gen/time-limit 15))
+                  (gen/log "waiting for quiescence")
+                  (gen/sleep 10)
+                  (gen/clients (gen/once client/bank-read)))
+    }
+  )
 )
 
 (defn -main
