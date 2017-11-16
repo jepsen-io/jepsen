24e588f4ba6842c9c11abf4c21fdf10305b2d41c
Ben Darnell <ben@bendarnell.com>
2017-11-16 16:45:19 -0500

    charybdefs: New subproject for disk fault injection
    Currently builds and installs everything and provides some basic
    controls for injecting faults. Future work includes building an
    actual nemesis with this and providing more fine-grained controls.

---
+++ charybdefs/project.clj
@@ -0,0 +1,8 @@
+(defproject jepsen-charybdefs "0.1.0-SNAPSHOT"
+  :description "charybdefs wrapper for use in jepsen"
+  :url "https://github.com/jepsen.io/jepsen"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [jepsen "0.1.6"]
+                 [yogthos/config "0.8"]])
