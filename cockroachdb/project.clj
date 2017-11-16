9008d1e1ccde885fc71080d807a39e5f235393e3
Ben Darnell <ben@bendarnell.com>
2017-11-16 14:33:47 -0500

    cockroachdb: Fix build by depending on jepsen 0.1.6

--- cockroachdb/project.clj
+++ cockroachdb/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [jepsen "0.1.5"]
+                 [jepsen "0.1.6"]
                  [org.clojure/java.jdbc "0.6.1"]
                  [circleci/clj-yaml "0.5.5"]
                  [org.postgresql/postgresql "9.4.1211"]]
