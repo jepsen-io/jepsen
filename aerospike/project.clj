23222ed1b7673ea2a92287e2795eadc5a1548804
Aphyr <aphyr@aphyr.com>
2017-11-17 15:52:45 -0600

    Aerospike: upgrade to jepsen 0.1.7-SNAPSHOT

--- aerospike/project.clj
+++ aerospike/project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main aerospike.core
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [jepsen "0.1.6"]
+                 [jepsen "0.1.7-SNAPSHOT"]
                  [io.jepsen/aerospike-client "4.1.0"]
                  [clj-wallhack "1.0.1"]])
