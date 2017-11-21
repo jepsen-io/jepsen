74a8b32eab800fb3d2080b0e4f80eb7ccc7d3f61
Aphyr <aphyr@aphyr.com>
2017-11-21 14:19:07 -0600

    Rewrite jepsen.core workers and nemesis
    Jepsen had longstanding issues with error recovery and
    concurrency; unexpected crashes in generators, clients, or the nemesis could result
    in tests that deadlock forever, failing to report the underlying fault.
    This patch rewrites worker concurrency essentially from scratch.
    
    It slightly changes the order of events: nemesis and client setup now
    occur *concurrently*, rather than the nemesis going first. An exception
    in any client or nemesis worker results in the entire test aborting,
    cleanly closing all clients, and that original exception being
    thrown through jepsen.core/run!.
    
    Users should see improved stability in tests. I hope. It's a big change,
    so bear with me. :)

--- jepsen/src/jepsen/generator.clj
+++ jepsen/src/jepsen/generator.clj
@@ -14,13 +14,25 @@
   (:require [jepsen.util :as util]
             [knossos.history :as history]
             [clojure.core :as c]
-            [clojure.tools.logging :refer [warn info]])
+            [clojure.tools.logging :refer [warn info]]
+            [slingshot.slingshot :refer [throw+]])
   (:import (java.util.concurrent.atomic AtomicBoolean)
            (java.util.concurrent.locks LockSupport)
            (java.util.concurrent CyclicBarrier)))
 
 (defprotocol Generator
   (op [gen test process] "Yields an operation to apply."))
+
+(defn op-and-validate
+  "Wraps `op` to ensure we produce a valid operation for our
+  worker. Re-throws any exception we catch from the generator."
+  [gen test process]
+  (let [op (op gen test process)]
+    (when-not (or (nil? op) (map? op))
+      (throw+ {:type :invalid-op
+               :gen  gen
+               :op   op}))
+    op))
 
 (extend-protocol Generator
   nil
@@ -442,16 +454,3 @@
   "When the given generator completes, synchronizes, then yields nil."
   [gen]
   (->> gen (then void)))
-
-(defn op-and-validate
-  "Wraps `jepsen.generator/op` to ensure we produce a valid operation for our worker.
-  Re-throws any exception we catch from the generator."
-  [gen test process]
-  (try
-    (let [op (op gen test process)]
-      (assert (or (nil? op) (map? op))
-              (str "Expected an operation map from " gen ", but got " (pr-str op) " instead."))
-      op)
-    (catch Exception e
-      (warn "Generator" gen "threw")
-      (throw e))))
