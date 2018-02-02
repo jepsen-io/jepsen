62c156137d7d5ed7aa8dfabd7522f2bc319058b7
Aphyr <aphyr@aphyr.com>
2018-02-01 19:10:03 -0600

    Aerospike: 0.1.0

--- aerospike/project.clj
+++ aerospike/project.clj
@@ -1,10 +1,10 @@
-(defproject aerospike "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject aerospike "0.1.0"
+  :description "Jepsen tests for the Aerospike database."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main aerospike.core
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [jepsen "0.1.7-SNAPSHOT"]
+                 [jepsen "0.1.7"]
                  [io.jepsen/aerospike-client "4.1.0"]
                  [clj-wallhack "1.0.1"]])
