baed5a8f01d08a0647ea7a7822f5661d8e5ea86a
Aphyr <aphyr@aphyr.com>
2018-02-05 00:24:57 -0600

    Jepsen: clojure 1.9.0, dom-top 1.0.2, knossos 0.3.3

--- jepsen/project.clj
+++ jepsen/project.clj
@@ -1,12 +1,12 @@
 (defproject jepsen "0.1.8-SNAPSHOT"
   :description "Call Me Maybe: Network Partitions in Practice"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/data.fressian "0.2.1"]
                  [org.clojure/tools.logging "0.3.1"]
                  [spootnik/unilog "0.7.13"]
                  [org.clojure/tools.cli "0.3.5"]
                  [clj-time "0.11.0"]
-                 [knossos "0.3.2" :exclusions [org.slf4j/slf4j-log4j12]]
+                 [knossos "0.3.3" :exclusions [org.slf4j/slf4j-log4j12]]
                  [clj-ssh "0.5.14"]
                  [gnuplot "0.1.1"]
                  [http-kit "2.1.18"]
@@ -14,7 +14,7 @@
                  [hiccup "1.0.5"]
                  [org.clojars.achim/multiset "0.1.0"]
                  [byte-streams "0.2.2"]
-                 [dom-top "1.0.2-SNAPSHOT"]
+                 [dom-top "1.0.2"]
                  [slingshot "0.12.2"]]
   :main jepsen.cli
   :plugins [[lein-localrepo "0.5.4"]
@@ -24,7 +24,8 @@
   :jvm-opts ["-Xmx32g" "-XX:+UseConcMarkSweepGC" "-XX:+UseParNewGC"
              "-XX:+CMSParallelRemarkEnabled" "-XX:+AggressiveOpts"
              "-XX:+UseFastAccessorMethods" "-server"
-             "-XX:-OmitStackTraceInFastThrow"]
+             ;"-XX:-OmitStackTraceInFastThrow"
+             ]
   :codox {:output-path "doc/"
           :source-uri "https://github.com/jepsen-io/jepsen/blob/{version}/jepsen/{filepath}#L{line}"
           :metadata {:doc/format :markdown}})
