50922c4a4723aa26a5936323230bef7eec36cce4
Aphyr <aphyr@aphyr.com>
2018-02-05 00:25:26 -0600

    Jepsen 0.1.8

--- jepsen/project.clj
+++ jepsen/project.clj
@@ -1,4 +1,4 @@
-(defproject jepsen "0.1.8-SNAPSHOT"
+(defproject jepsen "0.1.8"
   :description "Call Me Maybe: Network Partitions in Practice"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/data.fressian "0.2.1"]
