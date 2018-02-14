32fe1b30e5c952f868221462d9da2823a9e8da32
Aphyr <aphyr@aphyr.com>
2018-02-13 19:02:40 -0600

    dgraph: depend on jepsen 0.1.9-SNAPSHOT

--- dgraph/project.clj
+++ dgraph/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [jepsen "0.1.8"]
+                 [jepsen "0.1.9-SNAPSHOT"]
                  [clj-http "3.7.0"]
                  [cheshire "5.8.0"]
                  ; Note that we specify manual versions of dgraph deps since
@@ -15,4 +15,5 @@
                  [io.grpc/grpc-core "1.7.0"]
                  [io.netty/netty-codec-http2 "4.1.16.Final"]
                  [clj-wallhack "1.0.1"]]
-  :main jepsen.dgraph.core)
+  :main jepsen.dgraph.core
+  :profiles {:uberjar {:aot :all}})
