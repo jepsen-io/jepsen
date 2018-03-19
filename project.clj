e692f0cd6f1e47d8794e94ea65fcaa513bff053d
Brandon Mitchell <brandon@thewholedoubt.com>
2018-03-19 10:42:57 -0700

    Install, configure, start, and stop a single-node FaunaDB cluster.

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main jepsen.fauna
   :dependencies [[org.clojure/clojure "1.9.0"]
+                 [io.forward/yaml "1.0.7"]
                  [jepsen "0.1.8"]
                  [com.faunadb/faunadb-java "1.2.0"]])
--- src/jepsen/fauna.clj
+++ src/jepsen/fauna.clj
@@ -1,14 +1,20 @@
 (ns jepsen.fauna
-  (:require [clojure.tools.logging :refer :all]
+  (:require [clojure.java.io :as io]
+            [clojure.tools.logging :refer :all]
             [clojure.string :as str]
             [jepsen [cli :as cli]
              [control :as c :refer [|]]
              [db :as db]
              [tests :as tests]]
             [jepsen.control.util :as cu]
-            [jepsen.os.debian :as debian]))
+            [jepsen.os.debian :as debian]
+            [yaml.core :as yaml]))
 
-(def key
+(def root-key
+  "Administrative key for the FaunaDB cluster."
+  "secret")
+
+(def repo-key
   "FaunaDB dpkg repository key."
   "TPwTIfv9rYCBsY9PR2Y31F1X5JEUFIifWopdM3RvdHXaLgjkOl0wPoNp1kif1hJS")
 
@@ -17,10 +23,32 @@
   [version]
   (debian/install-jdk8!)
   (debian/add-repo! "faunadb"
-                    (str/join ["deb [arch=all] https://" key "@repo.fauna.com/enterprise/debian unstable non-free"]))
+                    (str/join ["deb [arch=all] https://" repo-key "@repo.fauna.com/enterprise/debian unstable non-free"]))
   (c/su (c/exec :wget :-qO :- "https://repo.fauna.com/faunadb-gpg-public.key" |
                 :apt-key :add :-))
   (debian/install {"faunadb" version}))
+
+(defn configure!
+  "Configure FaunaDB."
+  [test node]
+  (info "Configuring" node)
+  (c/su
+   (c/exec :echo (-> "faunadb.conf"
+                     io/resource
+                     slurp)
+           :> "/etc/init/faunadb.conf")
+   (c/exec :echo
+           (yaml/generate-string
+            (merge (yaml/parse-string
+                    (-> "faunadb.yml"
+                        io/resource
+                        slurp))
+                   {:auth_root_key root-key
+                    :network_broadcast_address node
+                    :network_host_id node
+                    :network_listen_address node
+                    :storage_transaction_log_nodes [[node]]}))
+            :> "/etc/faunadb.yml")))
 
 (defn db
   "FaunaDB for a particular version."
@@ -28,10 +56,15 @@
   (reify db/DB
     (setup! [_ test node]
       (info node "installing FaunaDB" version)
-      (install! version))
+      (install! version)
+      (configure! test node)
+      (c/su (c/exec :initctl :start :faunadb)))
 
     (teardown! [_ test node]
-      (info node "tearing down FaunaDB"))))
+      (info node "tearing down FaunaDB")
+      (c/su
+       (c/exec :initctl :stop :faunadb)
+       (c/exec :rm :-rf "/var/lib/faunadb")))))
 
 (defn fauna-test
   "Given an options map from the command line
