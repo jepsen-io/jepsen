9625238a1e8f14166e1401964092eb67c05545d1
Aphyr <aphyr@aphyr.com>
2018-04-10 10:18:42 -0500

    jepsen.tests.bank: plot showing balances over time

--- jepsen/src/jepsen/tests/bank.clj
+++ jepsen/src/jepsen/tests/bank.clj
@@ -10,7 +10,12 @@
   (:require [knossos.op :as op]
             [clojure.core.reducers :as r]
             [jepsen [generator :as gen]
-                    [checker :as checker]]))
+                    [checker :as checker]
+                    [store :as store]
+                    [util :as util]]
+            [jepsen.checker.perf :as perf]
+            [knossos.history :as history]
+            [gnuplot.core :as g]))
 
 (defn read
   "A generator of read operations."
@@ -63,6 +68,53 @@
         {:valid? (empty? bad-reads)
          :bad-reads bad-reads}))))
 
+(defn by-node
+  "Groups operations by node."
+  [test history]
+  (let [nodes (:nodes test)
+        n     (count nodes)]
+    (group-by (fn [op]
+                (let [p (:process op)]
+                  (if (number? p)
+                    (nth nodes (mod p n))
+                    p)))
+              history)))
+
+(defn points
+  "Turns a history into a seqeunce of [time total-of-accounts] points."
+  [history]
+  (->> history
+       (r/filter op/ok?)
+       (r/filter #(= :read (:f %)))
+       (r/map (fn [op]
+                [(util/nanos->secs (:time op))
+                 (reduce + (vals (:value op)))]))
+       (into [])))
+
+(defn plotter
+  "Renders a graph of balances over time"
+  []
+  (reify checker/Checker
+    (check [this test model history opts]
+      (let [totals (->> history
+                        (by-node test)
+                        (util/map-vals points))
+            path (.getCanonicalPath
+                   (store/path! test (:subdirectory opts) "bank.png"))]
+        (try
+          (g/raw-plot!
+            (concat (perf/preamble path)
+                    [['plot (apply g/list
+                                   (for [[node points] totals]
+                                     ["-"
+                                      'with       'points
+                                      'pointtype  2
+                                      'title      (name node)]))]])
+            (vals totals))
+          {:valid? true}
+          (catch java.io.IOException _
+                 (throw (IllegalStateException. "Error rendering plot, verify gnuplot is installed and reachable"))))))))
+
 (defn test
   "A partial test; bundles together some default choices for keys and amounts
   with a generator and checker."
@@ -70,5 +122,6 @@
   {:max-transfer  5
    :total-amount  100
    :accounts      (vec (range 8))
-   :checker       (checker)
+   :checker       (checker/compose {:SI    (checker)
+                                    :plot  (plotter)})
    :generator     (generator)})
