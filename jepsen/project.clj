c667e35f20c581b25287c5453dd4ca0e212d29b7
Aphyr <aphyr@aphyr.com>
2018-05-24 18:02:18 -0500

    Whoops, bump dom-top dep

--- jepsen/project.clj
+++ jepsen/project.clj
@@ -16,7 +16,7 @@
                  [hiccup "1.0.5"]
                  [org.clojars.achim/multiset "0.1.0"]
                  [byte-streams "0.2.2"]
-                 [dom-top "1.0.3-SNAPSHOT"]
+                 [dom-top "1.0.3"]
                  [slingshot "0.12.2"]
                  [org.clojure/data.codec "0.1.1"]]
   :main jepsen.cli
