f345226dba1266bc37487d734a02caddf7d1d125
Aphyr <aphyr@aphyr.com>
2018-07-02 14:01:53 -0500

    jepsen 0.1.10

--- jepsen/project.clj
+++ jepsen/project.clj
@@ -1,5 +1,5 @@
 (defproject jepsen "0.1.10"
-  :description "Distributed systems testing framework"
+  :description "Distributed systems testing framework."
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
