1fff64ad1a0af4a89dd2dcd32bdeb4bace6faf7c
Aphyr <aphyr@aphyr.com>
2019-01-28 19:26:43 -0500

    Yugabyte: multi-key-acid test review
    This test used identical values for both keys during writes, rather than
    arbitrary values, and created a very large state space. It also only
    tested 100 operations *ever*: most of the time it did nothing. We've
    moved to a more compact but randomized state space, and updated the test
    to use `jepsen.independent`, so it continues testing over the course of
    an entire test.

--- yugabyte/src/yugabyte/multi_key_acid.clj
+++ yugabyte/src/yugabyte/multi_key_acid.clj
@@ -11,14 +11,9 @@
                                      [policies :refer :all]
                                      [cql :as cql]]
             [yugabyte [core :refer :all]
+                      [client :as c]
                       [auto :as auto]])
-  (:import (knossos.model Model)
-           (com.datastax.driver.core.exceptions DriverException
-                                                UnavailableException
-                                                OperationTimedOutException
-                                                ReadTimeoutException
-                                                WriteTimeoutException
-                                                NoHostAvailableException)))
+  (:import (knossos.model Model)))
 
 (def setup-lock (Object.))
 (def table-name "multi_key_acid")
@@ -28,23 +23,28 @@
   Model
   (step [this op]
         (assert (= (:f op) :txn))
-        ; `knossos.model.memo/memo` function passing operation invocations as steps to the model, so in our case we can
-        ; receive operation which value is simply `:read` without any sequence and need to handle that properly by
-        ; returning current state.
-        (if (= (:value op) :read) this
-        (reduce
-          (fn [state [f k v]]
-            ; Apply this particular op
-            (case f
-              :read  (if (or (nil? v)
-                             (= v (get state k)))
-                       state
-                       (reduced
-                        (model/inconsistent
-                         (str k ": " (pr-str (get state k)) "≠" (pr-str v)))))
-              :write (assoc state k v)))
+        ; `knossos.model.memo/memo` function passing operation invocations as
+        ; steps to the model, so in our case we can receive operation which
+        ; value is simply `:read` without any sequence and need to handle that
+        ; properly by returning current state.
+        ;
+        ; TODO: this feels... deeply illegal. Figure out why this seems to work
+        ; OK anyway?
+        (if (= (:value op) :read)
           this
-          (:value op)))))
+          (reduce
+            (fn [state [f k v]]
+              ; Apply this particular op
+              (case f
+                :read  (if (or (nil? v)
+                               (= v (get state k)))
+                         state
+                         (reduced
+                           (model/inconsistent
+                             (str k ": " (pr-str (get state k)) "≠" (pr-str v)))))
+                :write (assoc state k v)))
+            this
+            (:value op)))))
 
 (defn multi-register
   "A register supporting read and write transactions over registers identified
@@ -54,107 +54,83 @@
   [values]
   (map->MultiRegister values))
 
-(defrecord CQLMultiKey [conn]
-  client/Client
-  (open! [this test node]
-    (info "Opening connection to " node)
-    (assoc this :conn (cassandra/connect [node] {:protocol-version 3
-                                                 :retry-policy (retry-policy :no-retry-on-client-timeout)})))
+(c/defclient CQLMultiKey []
   (setup! [this test]
-          (locking setup-lock
-            (cql/create-keyspace conn keyspace
-                                 (if-not-exists)
-                                 (with
-                                   {:replication
-                                    {"class"              "SimpleStrategy"
-                                     "replication_factor" 3}}))
-            (cassandra/execute conn (str "CREATE TABLE IF NOT EXISTS " keyspace "." table-name
-                                         " (id INT PRIMARY KEY, val INT) WITH transactions = { 'enabled' : true }"))
-            (->CQLMultiKey conn)))
+    (locking setup-lock
+      (cql/create-keyspace conn keyspace
+                           (if-not-exists)
+                           (with
+                             {:replication
+                              {"class"              "SimpleStrategy"
+                               "replication_factor" 3}}))
+      (cassandra/execute
+        conn (str "CREATE TABLE IF NOT EXISTS "
+                  keyspace "." table-name
+                  ; ik is an independent-key, so we can segment the test into
+                  ; independent groups. group is a reserved keyword. :(
+                  " (id INT, ik INT, val INT,"
+                  " PRIMARY KEY (id, ik)"
+                  ") WITH transactions = { 'enabled' : true }"))))
+
   (invoke! [this test op]
-           (assert (= (:f op) :txn))
-           (let [value (:value op)]
-             (if (= value :read)
-               (try (auto/wait-for-recovery 30 conn)
-                 (assoc op :type :ok :value
-                                 (->> (cql/select-with-ks conn keyspace table-name)
-                                      (map (fn [x] [:read (:id x) (:val x)]))
-                                      (sort-by #(second %))))
-                 (catch UnavailableException e
-                   (info "Not enough replicas - failing")
-                   (assoc op :type :fail :error (.getMessage e)))
-                 (catch ReadTimeoutException e
-                   (assoc op :type :fail :error :read-timed-out))
-                 (catch OperationTimedOutException e
-                   (assoc op :type :fail :error :client-timed-out))
-                 (catch NoHostAvailableException e
-                   (info "All nodes are down - sleeping 2s")
-                   (Thread/sleep 2000)
-                   (assoc op :type :fail :error (.getMessage e))))
-               (try
-                 ; TODO - temporarily replaced by single DB call until YugaByteDB supports transaction spanning
-                 ; multiple DB calls.
-;                 (cassandra/execute conn "BEGIN TRANSACTION")
-;                 (doseq [[sub-op id val] value]
-;                   (assert (= sub-op :write))
-;                   (cql/insert conn table-name {:id id :val val}))
-;                 (cassandra/execute conn "END TRANSACTION;")
-                 (cassandra/execute conn
-                                    (str "BEGIN TRANSACTION "
-                                         (->>
-                                          (for [[sub-op id val] value]
-                                            (do
-                                              (assert (= sub-op :write))
-                                              (str "INSERT INTO " keyspace "." table-name " (id, val) VALUES ("
-                                                   id ", " val ");")))
-                                          clojure.string/join)
-                                         "END TRANSACTION;"))
-                 (assoc op :type :ok)
-                 (catch UnavailableException e
-                   (assoc op :type :fail :error (.getMessage e)))
-                 (catch WriteTimeoutException e
-                   (assoc op :type :info :error :write-timed-out))
-                 (catch OperationTimedOutException e
-                   (assoc op :type :info :error :client-timed-out))
-                 (catch NoHostAvailableException e
-                   (info "All nodes are down - sleeping 2s")
-                   (Thread/sleep 2000)
-                   (assoc op :type :fail :error (.getMessage e)))
-                 (catch DriverException e
-                   (if (re-find #"Value write after transaction start|Conflicts with higher priority transaction|Conflicts with committed transaction|Operation expired: Failed UpdateTransaction.* status: COMMITTED .*: Transaction expired"
-                                (.getMessage e))
-                     ; Definitely failed
-                     (assoc op :type :fail :error (.getMessage e))
-                     (throw e)))))))
-  (teardown! [this test])
-  (close! [this test]
-          (info "Closing client with conn" conn)
-          (cassandra/disconnect! conn)))
+    (assert (= (:f op) :txn))
+    (let [[ik value] (:value op)]
+      (if (= value :read)
+        (assoc op :type :ok :value
+               (->> (cql/select-with-ks conn keyspace table-name
+                                        (where [[= :ik ik ]]))
+                    (map (fn [x] [:read (:id x) (:val x)]))
+                    (sort-by second)
+                    (independent/tuple ik)))
+
+        ; TODO - temporarily replaced by single DB call until YugaByteDB supports transaction spanning
+        ; multiple DB calls.
+        ;                 (cassandra/execute conn "BEGIN TRANSACTION")
+        ;                 (doseq [[sub-op id val] value]
+        ;                   (assert (= sub-op :write))
+        ;                   (cql/insert conn table-name {:id id :val val}))
+        ;                 (cassandra/execute conn "END TRANSACTION;")
+        (do (cassandra/execute conn
+                               (str "BEGIN TRANSACTION "
+                                    (->> (for [[sub-op id val] value]
+                                           (do
+                                             (assert (= sub-op :write))
+                                             (str "INSERT INTO "
+                                                  keyspace "." table-name
+                                                  " (id, ik, val) VALUES ("
+                                                  id ", " ik ", " val ");")))
+                                         clojure.string/join)
+                                    "END TRANSACTION;"))
+            (assoc op :type :ok)))))
+
+  (teardown! [this test]))
 
 (defn r [_ _] {:type :invoke, :f :txn :value :read})
+
 (defn w [_ _]
-  (let [val (rand-int 10)]
-    {:type  :invoke,
-     :f     :txn
-     :value [[:write (rand-int 10) val] [:write (rand-int 10) val]]}))
+  {:type  :invoke,
+   :f     :txn
+   :value [[:write (rand-int 3) (rand-int 5)]
+           [:write (rand-int 3) (rand-int 5)]]})
 
 (defn test
   [opts]
   (yugabyte-test
-   (let [concurrency (max 10 (:concurrency opts))]
+   (let [n (count (:nodes opts))]
      (merge opts
             {:name             "multi-key-acid"
              :client           (CQLMultiKey. nil)
-             :concurrency      concurrency
-             :client-generator (->>
-                                (gen/reserve (quot concurrency 2) r w)
-                                (gen/delay-til 1/2)
-                                (gen/stagger 0.1)
-                                (gen/limit 100))
-             :client-final-generator (gen/once r)
+             :client-generator (independent/concurrent-generator
+                                 (* 2 n)
+                                 (range)
+                                 (fn [k]
+                                   (->> (gen/reserve n r w)
+                                        (gen/stagger 0.1)
+                                        (gen/limit 100))))
              :model            (multi-register {})
-             :checker
-             (checker/compose
-              {:perf     (checker/perf)
-               :timeline (timeline/html)
-               :linear   (checker/linearizable)})}))))
+             :checker (checker/compose
+                        {:perf     (checker/perf)
+                         :indep (independent/checker
+                                  (checker/compose
+                                    {:timeline (timeline/html)
+                                     :linear   (checker/linearizable)}))})}))))
