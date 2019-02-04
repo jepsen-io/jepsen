e4020519d26929d3349299c7e0cf71d57c85d547
Aphyr <aphyr@aphyr.com>
2019-02-04 15:09:30 -0500

    Yugabyte: remove superfluous single-row-insert test

--- yugabyte/src/yugabyte/runner.clj
+++ yugabyte/src/yugabyte/runner.clj
@@ -11,13 +11,11 @@
                       [multi-key-acid]
                       [nemesis :as nemesis]
                       [set :as set]
-                      [single-key-acid]
-                      [single-row-inserts]]))
+                      [single-key-acid]]))
 
 (def tests
   "A map of test names to test constructors."
-  {"single-row-inserts" yugabyte.single-row-inserts/test
-   "single-key-acid"    yugabyte.single-key-acid/test
+  {"single-key-acid"    yugabyte.single-key-acid/test
    "multi-key-acid"     yugabyte.multi-key-acid/test
    "counter-inc"        yugabyte.counter/test-inc
    "counter-inc-dec"    yugabyte.counter/test-inc-dec
