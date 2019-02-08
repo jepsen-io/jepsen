07c4c3cdfc3f4338139d6f44a6e7083d8bcd7256
Mikaela Patella <m.c.patella@gmail.com>
2019-02-08 12:32:44 -0800

    CI: let's try trusty, oraclejdk8, without flag exceptions

--- jepsen/project.clj
+++ jepsen/project.clj
@@ -32,7 +32,6 @@
              "-XX:+UseFastAccessorMethods" "-server"
              ;"-XX:-OmitStackTraceInFastThrow"
              ]
-  :profiles {:ci {:jvm-opts ["-Xmx32g" "-server"]}}
   :test-selectors {:default (complement :integration)
                    :integration :integration}
   :codox {:output-path "doc/"
