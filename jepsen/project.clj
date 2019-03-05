e384e0e96589756295a9800682e9c223d24c73d6
Jacob <jacob.romer@symbiont.io>
2019-03-05 00:39:17 -0500

    bump version

--- jepsen/project.clj
+++ jepsen/project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.jacobmbr/jepsen "0.1.1"
+(defproject org.clojars.jacobmbr/jepsen "0.1.2"
   :description "Distributed systems testing framework."
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
