10aef756d8fa83ae99e5e40d448a35760b082860
Aphyr <aphyr@aphyr.com>
2019-03-06 10:17:12 -0500

    0.1.13-SNAPSHOT

--- jepsen/project.clj
+++ jepsen/project.clj
@@ -1,4 +1,4 @@
-(defproject jepsen "0.1.12"
+(defproject jepsen "0.1.13-SNAPSHOT"
   :description "Distributed systems testing framework."
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
