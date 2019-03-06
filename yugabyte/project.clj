33f05048907923eae0189d29a2bec2c5e3b2641e
Aphyr <aphyr@aphyr.com>
2019-03-06 15:31:32 -0500

    Jepsen 0.1.12, readme

--- yugabyte/project.clj
+++ yugabyte/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [clj-http "3.8.0" :exclusions [commons-logging]]
-                 [jepsen "0.1.12-SNAPSHOT"]
+                 [jepsen "0.1.12"]
                  [clojurewerkz/cassaforte "3.0.0-alpha2-yb-SNAPSHOT"]
                  [org.slf4j/jcl-over-slf4j "1.7.25"]
                  [org.slf4j/jul-to-slf4j "1.7.25"]
