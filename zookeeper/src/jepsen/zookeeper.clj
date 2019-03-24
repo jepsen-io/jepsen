2a3c49eeb1d566028c60c7450de0b1333cb8ba5e
Alexander Abramov <alexander.abramov.pub@gmail.com>
2019-03-24 18:18:21 +0100

    Fix zookeeper version currently available on Debian

--- zookeeper/src/jepsen/zookeeper.clj
+++ zookeeper/src/jepsen/zookeeper.clj
@@ -112,7 +112,7 @@
          opts
          {:name    "zookeeper"
           :os      debian/os
-          :db      (db "3.4.5+dfsg-2")
+          :db      (db "3.4.9-3+deb9u1")
           :client  (client nil nil)
           :nemesis (nemesis/partition-random-halves)
           :generator (->> (gen/mix [r w cas])
