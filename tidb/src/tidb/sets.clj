53da5b489e49c733a8149cd287dcc0e6fff1839f
Aphyr <aphyr@aphyr.com>
2019-04-04 13:32:43 -0400

    TiDB: Set test review
    Reserve dedicated threads for reads and writes. Read continuously.
    Remove final generator. Use set-full checker. Remove transaction.

--- tidb/src/tidb/sets.clj
+++ tidb/src/tidb/sets.clj
@@ -16,38 +16,44 @@
   (setup! [this test]
     (j/execute! conn ["create table if not exists sets
                       (id     int not null primary key auto_increment,
-                      value bigint not null)"]))
+                       value  bigint not null)"]))
 
   (invoke! [this test op]
-    (with-txn op [c conn]
-      (case (:f op)
-        :add  (do (j/insert! c :sets (select-keys op [:value]))
-                  (assoc op :type :ok))
-        :read (->> (j/query c ["select * from sets"])
-                   (mapv :value)
-                   (into (sorted-set))
-                   (assoc op :type :ok, :value)))))
+    (c/with-error-handling op
+      (c/with-txn-aborts op
+        (case (:f op)
+          :add  (do (j/insert! conn :sets (select-keys op [:value]))
+                    (assoc op :type :ok))
+
+          :read (->> (j/query conn ["select * from sets"])
+                     (mapv :value)
+                     (assoc op :type :ok, :value))))))
 
   (teardown! [_ test])
 
   (close! [_ test]
     (c/close! conn)))
 
+(defn adds
+  []
+  (->> (range)
+       (map (fn [x] {:type :invoke, :f :add, :value x}))
+       (gen/seq)))
+
+(defn reads
+  []
+  {:type :invoke, :f :read, :value nil})
+
 (defn test
   [opts]
-  (basic/basic-test
-    (merge
-    {:name "set"
-     :client {:client (SetClient. nil)
-              :during (->> (range)
-                          (map (partial array-map
-                                        :type :invoke
-                                        :f :add
-                                        :value))
-                          gen/seq
-                          (gen/stagger 1))
-              :final (gen/once {:type :invoke, :f :read, :value nil})}
-     :checker (checker/compose
-                {:perf (checker/perf)
-                 :set  (checker/set)})}
-     opts)))
+  (let [c (:concurrency opts)]
+    (basic/basic-test
+      (merge
+        {:name "set"
+         :client {:client (SetClient. nil)
+                  :during (->> (gen/reserve (/ c 2) (adds) (reads))
+                               (gen/stagger 1/10))}
+         :checker (checker/compose
+                    {:perf (checker/perf)
+                     :set  (checker/set-full)})}
+        opts))))
