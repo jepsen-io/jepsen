03b61376e61c9d05dc1b3452a53bcdcb66cb5561
Aphyr <aphyr@aphyr.com>
2019-04-16 18:56:12 -0400

    TiDB: Polish off monotonic test rough edges

--- tidb/src/tidb/monotonic.clj
+++ tidb/src/tidb/monotonic.clj
@@ -21,13 +21,12 @@
                        (if (:use-index test) "pk" "sk") " = ?")
                   k])
       first
-      (:val -1)
-      (+ (rand-int 1))))
+      (:val -1)))
 
 (defn read-keys
   "Read several keys values from the table, returning a map of keys to values."
   [c test ks]
-  (->> (map (fn [k] (Thread/sleep 100) (read-key c test k)) ks)
+  (->> (map (partial read-key c test) ks)
        (zipmap ks)
        (into (sorted-map))))
   ;(zipmap ks (map (partial read-key c test) ks)))
@@ -47,25 +46,37 @@
         (c/create-index! conn ["create index cycle_sk_val on cycle (sk, val)"]))))
 
   (invoke! [this test op]
-    ;(c/with-txn op [c conn]
-    (let [c conn]
+    (c/with-txn op [c conn]
+    ;(let [c conn]
       (case (:f op)
         :read (let [v (read-keys c test (shuffle (keys (:value op))))]
                 (assoc op :type :ok, :value v))
-        :inc (let [k (:value op)
-                   v (read-key c test k)]
-               (if (= -1 v)
-                 (c/insert! c "cycle" {:pk k, :sk k, :val 0})
-                 (c/update! c "cycle" {:val (inc v)},
-                            [(str (if (:use-index test) "sk" "pk") " = ?")
-                             k]))
-               ; The monotonic value constraint isn't actually enough to
-               ; capture all the ordering dependencies here: an increment from
-               ; x->y must fall after every read of x, and before every read of
-               ; y, but the monotonic order relation can only enforce one of
-               ; those. We'll return the written value here. Still better than
-               ; nothing.
-               (assoc op :type :ok :value {k (inc v)})))))
+        :inc (let [k (:value op)]
+               (if (:update-in-place test)
+                 ; Update directly
+                 (do (when (= [0] (c/execute!
+                                    c [(str "update cycle set val = val + 1"
+                                            " where pk = ?") k]))
+                       ; That failed; insert
+                       (c/insert! c "cycle" {:pk k, :sk k, :val 0}))
+                     ; We can't place any constraints on the values since we
+                     ; didn't read anything
+                     (assoc op :type :ok, :value {}))
+
+                 ; Update via separate r/w
+                 (let [v (read-key c test k)]
+                   (if (= -1 v)
+                     (c/insert! c "cycle" {:pk k, :sk k, :val 0})
+                     (c/update! c "cycle" {:val (inc v)},
+                                [(str (if (:use-index test) "sk" "pk") " = ?")
+                                 k]))
+                   ; The monotonic value constraint isn't actually enough to
+                   ; capture all the ordering dependencies here: an increment
+                   ; from x->y must fall after every read of x, and before
+                   ; every read of y, but the monotonic order relation can only
+                   ; enforce one of those. We'll return the written value here.
+                   ; Still better than nothing.
+                   (assoc op :type :ok :value {k (inc v)})))))))
 
   (teardown! [this test])
 
@@ -86,13 +97,13 @@
 
 (defn workload
   [opts]
-  (let [key-count 2]
+  (let [key-count 8]
     {:client (IncrementClient. nil)
      :checker (checker/compose
                 {:cycle (cycle/checker
-                          cycle/monotonic-key-orders)
-;                          (cycle/combine cycle/monotonic-key-orders
-;                                                      cycle/process-orders))
+                          ;cycle/monotonic-key-orders)
+                          (cycle/combine cycle/monotonic-key-orders
+                                         cycle/process-orders))
                  :timeline (timeline/html)})
      :generator (->> (gen/mix [(incs key-count)
                                (reads key-count)]))}))
