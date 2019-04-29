8a28b7ba4d471bf565efda17e06093106ab488f8
Aphyr <aphyr@aphyr.com>
2019-04-29 10:58:42 -0400

    jepsen.txn/reduce-mops: reductions over micro-ops in a history.

--- txn/src/jepsen/txn.clj
+++ txn/src/jepsen/txn.clj
@@ -2,6 +2,20 @@
   "Manipulates transactions. Transactions are represented as a sequence of
   micro-operations (mops for short).")
 
+(defn reduce-mops
+  "Takes a history of operations, where each operation op has a :value which is
+  a transaction made up of [f k v] micro-ops. Runs a reduction over every
+  micro-op, where the reduction function is of the form (f state op [f k v]).
+  Saves you having to do endless nested reduces."
+  [f init-state history]
+  (reduce (fn op [state op]
+            (reduce (fn mop [state mop]
+                      (f state op mop))
+                    state
+                    (:value op)))
+          init-state
+          history))
+
 (defn ext-reads
   "Given a transaction, returns a map of keys to values for its external reads:
   values that transaction observed which it did not write itself."
