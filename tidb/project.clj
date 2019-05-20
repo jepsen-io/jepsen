894e6636e3b2a1346a497bffdad89fc1c95a8d24
Aphyr <aphyr@aphyr.com>
2019-05-20 11:53:32 -0400

    TiDB 0.1.0

--- tidb/project.clj
+++ tidb/project.clj
@@ -1,11 +1,11 @@
-(defproject tidb "0.1.0-SNAPSHOT"
+(defproject tidb "0.1.0"
   :description "Jepsen testing for TiDB"
   :url "https://pingcap.com/index"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main tidb.core
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [jepsen "0.1.14-SNAPSHOT"]
+                 [jepsen "0.1.14"]
                  [org.clojure/java.jdbc "0.7.9"]
                  [org.mariadb.jdbc/mariadb-java-client "2.4.1"]]
   :aot [tidb.core clojure.tools.logging.impl])
