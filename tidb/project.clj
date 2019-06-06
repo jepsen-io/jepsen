7eb62d2b080d15d2e9946bc80cc0c4046f7dd7a7
Aphyr <aphyr@aphyr.com>
2019-06-06 18:24:01 -0400

    TiDB: jepsen 0.1.15-SNAPSHOT

--- tidb/project.clj
+++ tidb/project.clj
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [clj-http "3.10.0"]
                  [cheshire "5.8.1"]
-                 [jepsen "0.1.14"]
+                 [jepsen "0.1.15-SNAPSHOT"]
                  [org.clojure/java.jdbc "0.7.9"]
                  [org.mariadb.jdbc/mariadb-java-client "2.4.1"]]
   :aot [tidb.core clojure.tools.logging.impl])
