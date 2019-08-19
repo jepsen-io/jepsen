fc23d83751d6eea14fcebf03c1a592bba6294967
Aphyr <aphyr@aphyr.com>
2019-08-19 10:06:02 -0400

    YugaByte: append back to primary keys

--- yugabyte/src/yugabyte/ysql/append.clj
+++ yugabyte/src/yugabyte/ysql/append.clj
@@ -30,6 +30,53 @@
   [test k]
   (table-name (mod (hash k) (table-count test))))
 
+(defn read-primary
+  "Reads a key based on primary key"
+  [conn table k]
+  (some-> conn
+          (c/query [(str "select (v) from " table " where k = ?") k])
+          first
+          :v
+          (str/split #",")
+          (->> (mapv #(Long/parseLong %)))))
+
+(defn append-primary!
+  "Writes a key based on primary key."
+  [conn table k v]
+  (let [r (c/execute! conn [(str "update " table
+                                 " set v = CONCAT(v, ',', ?) "
+                                 "where k = ?") v k])]
+    (when (= [0] r)
+      ; No rows updated
+      (c/execute! conn
+                  [(str "insert into " table
+                        "(k, k2, v) values (?, ?, ?)") k k v]))
+    v))
+
+(defn read-secondary
+  "Reads a key based on a predicate over a secondary key, k2"
+  [conn table k]
+  (some-> conn
+          (c/query [(str "select (v) from " table " where (k2 * 2) - ? = ?")
+                    k k])
+          first
+          :v
+          (str/split #",")
+          (->> (mapv #(Long/parseLong %)))))
+
+(defn append-secondary!
+  "Writes a key based on a predicate over a secondary key, k2. Returns v."
+  [conn table k v]
+  (let [r (c/execute! conn [(str "update " table
+                                 " set v = CONCAT(v, ',', ?) "
+                                 "where (k2 * 2) - ? = ?") v k k])]
+    (when (= [0] r)
+      ; No rows updated
+      (c/execute! conn
+                  [(str "insert into " table
+                        "(k, k2, v) values (?, ?, ?)") k k v]))
+    v))
+
 (defn mop!
   "Executes a transactional micro-op of the form [f k v] on a connection, where
   f is either :r for read or :append for list append. Returns the completed
@@ -37,26 +84,8 @@
   [conn test [f k v]]
   (let [table (table-for test k)]
     [f k (case f
-           :r (some-> conn
-                      (c/query [(str "select (v) from " table
-                                     " where (k2 * 2) - ? = ?") k k])
-                                     ; " where k = ?") k])
-                      first
-                      :v
-                      (str/split #",")
-                      (->> (mapv #(Long/parseLong %))))
-
-           :append
-           (let [r (c/execute! conn
-                               [(str "update " table
-                                     " set v = CONCAT(v, ',', ?) "
-                                     "where (k2 * 2) - ? = ?") v k k])]
-             (when (= [0] r)
-               ; No rows updated
-               (c/execute! conn
-                           [(str "insert into " table
-                                 "(k, k2, v) values (?, ?, ?)") k k v]))
-             v))]))
+           :r       (read-primary conn table k)
+           :append  (append-primary! conn table k v))]))
 
 (defrecord InternalClient []
   c/YSQLYbClient
