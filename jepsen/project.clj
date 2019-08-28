d36de0f140dceef878f5b8764a26703436c41d9b
Aphyr <aphyr@aphyr.com>
2019-08-28 17:23:19 -0400

    0.1.15

--- jepsen/project.clj
+++ jepsen/project.clj
@@ -1,4 +1,4 @@
-(defproject jepsen "0.1.15-SNAPSHOT"
+(defproject jepsen "0.1.15"
   :description "Distributed systems testing framework."
   :url         "https://jepsen.io"
   :license {:name "Eclipse Public License"
