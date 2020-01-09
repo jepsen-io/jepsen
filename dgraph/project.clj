73422565571dd65485fe057a52cb9def463b58c6
Aphyr <aphyr@aphyr.com>
2020-01-09 09:02:37 -0500

    Dgraph: clean up dependency tree by adding exclusions

--- dgraph/project.clj
+++ dgraph/project.clj
@@ -12,13 +12,19 @@
                  ; grpc uses version ranges
                  [io.dgraph/dgraph4j "2.0.2"
                   :exclusions [io.grpc/grpc-core
-                               io.netty/netty-codec-http2]]
+                               io.grpc/grpc-context
+                               io.netty/netty-codec-http2
+                               com.google.errorprone/error_prone_annotations
+                               org.codehaus.mojo/animal-sniffer-annotations]]
                  [io.grpc/grpc-core "1.22.1"
                   :exclusions [com.google.errorprone/error_prone_annotations
                                io.grpc/grpc-api]]
                  [io.netty/netty-codec-http2 "4.1.39.Final"]
                  [io.opencensus/opencensus-api  "0.23.0"]
                  [io.opencensus/opencensus-impl "0.23.0"]
-                 [io.opencensus/opencensus-exporter-trace-jaeger "0.23.0"]]
+                 [io.opencensus/opencensus-exporter-trace-jaeger "0.23.0"
+                  :exclusions [org.slf4j/slf4j-api
+                               com.google.code.gson/gson]]]
+
   :main jepsen.dgraph.core
   :profiles {:uberjar {:aot :all}})
