df14b7c22d5aed9ccb34dbcab73d1325b092cdee
Aphyr <aphyr@aphyr.com>
2020-05-28 14:54:53 -0400

    client/open-compat and friends are now gone

--- jepsen/src/jepsen/client.clj
+++ jepsen/src/jepsen/client.clj
@@ -36,21 +36,6 @@
     (open!     [this test node] this)
     (close!    [this test])))
 
-(defn open-compat!
-  "Attempts to call `open!` on the given client. If `open!` does not
-  exist, we assume a legacy implementation of `setup!`."
-  [client test node]
-  (try
-    (let [client (open! client test node)
-          _      (setup! client test)]
-      (assert client (str "Expected a client, but `open!` returned " (pr-str client) " instead."))
-      client)
-    (catch java.lang.AbstractMethodError e
-      (warn "DEPRECATED: `jepsen.client/open!` not implemented. Falling back to deprecated semantics of `jepsen.client/setup!`. You should separate your client's `setup!` function into `open!` and `setup!`. See the jepsen.client documentation for details.")
-      (let [client (setup! client test node)]
-        (assert client (str "Expected a client, but `setup!` returned " (pr-str client) " instead."))
-        client))))
-
 (defn closable?
   "Returns true if the given client implements method `close!`."
   [client]
@@ -59,16 +44,6 @@
        :members
        (map :name)
        (some #{'close_BANG_})))
-
-(defn close-compat!
-  "Inspects the client for `close!` method and calls `teardown!` then `close!`.
-  If `close!` is not implemented, we assume a legacy implementation of `teardown!`."
-  [client test]
-  (if (closable? client)
-    (do (teardown! client test)
-        (close! client test))
-    (do (warn "DEPRECATED: `jepsen.client/close!` not implemented. Falling back to deprecated semantics of `jepsen.client/teardown!`. You should separate your client's `teardown!` function into `close!` and `teardown!`. See the jepsen.client documentation for details.")
-        (teardown! client test))))
 
 (defrecord Validate [client]
   Client
