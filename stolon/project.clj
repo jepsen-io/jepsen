a4e6256b7823cb7d259929c4fd2670b8e7b3cc3c
Aphyr <aphyr@aphyr.com>
2020-06-11 09:45:53 -0400

    version 0.1.0: jepsen 0.2.0, etcd 0.2.1

--- stolon/project.clj
+++ stolon/project.clj
@@ -1,11 +1,11 @@
-(defproject jepsen.stolon "0.1.0-SNAPSHOT"
+(defproject jepsen.stolon "0.1.0"
   :description "Jepsen tests for Stolon, a PostgreSQL HA system."
   :url "https://github.com/jepsen-io/jepsen"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [jepsen "0.2.0-SNAPSHOT"]
-                 [jepsen.etcd "0.2.1-SNAPSHOT"]
+                 [jepsen "0.2.0"]
+                 [jepsen.etcd "0.2.1"]
                  [seancorfield/next.jdbc "1.0.445"]
                  [org.postgresql/postgresql "42.2.12"]
                  [cheshire "5.10.0"]
