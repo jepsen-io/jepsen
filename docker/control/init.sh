0ae8f1e37208480bc4aae8fe28c2e0541f0d6523
Aphyr <aphyr@aphyr.com>
2021-01-05 14:26:34 -0500

    Docker: bin/up now supports -n NUMBER, for a configurable number of DB nodes

--- docker/control/init.sh
+++ docker/control/init.sh
@@ -9,9 +9,9 @@ if [ ! -f ~/.ssh/known_hosts ]; then
     chmod 600 ~/.ssh/id_rsa
     echo $SSH_PUBLIC_KEY > ~/.ssh/id_rsa.pub
     echo > ~/.ssh/known_hosts
-    for f in $(seq 1 5); do
-      ssh-keyscan -t rsa n$f >> ~/.ssh/known_hosts
-    done
+    while read node; do
+      ssh-keyscan -t rsa $node >> ~/.ssh/known_hosts
+    done </var/jepsen/shared/nodes
 fi
 
 # TODO: assert that SSH_PRIVATE_KEY==~/.ssh/id_rsa
