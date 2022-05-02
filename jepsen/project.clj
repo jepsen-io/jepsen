d7ece2d01c86a8e08e31d6db68fdc44a6889cc84
Aphyr <aphyr@aphyr.com>
2022-05-02 18:22:12 -0400

    Elle 0.1.5-SNAPSHOT

--- jepsen/project.clj
+++ jepsen/project.clj
@@ -8,7 +8,7 @@
                  [org.clojure/tools.logging "1.2.4"]
                  [org.clojure/tools.cli "1.0.206"]
                  [spootnik/unilog "0.7.29"]
-                 [elle "0.1.4"]
+                 [elle "0.1.5-SNAPSHOT"]
                  [clj-time "0.15.2"]
                  [jepsen.txn "0.1.2"]
                  [knossos "0.3.8"]
