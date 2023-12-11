16309b64e593249a0b5ea0c0311155899815a3bf
Aerospike-NateL <nlarsen@aerospike.com>
2023-12-11 13:25:26 -0800

    aerospike.project - use published client with added dependencies

--- aerospike/project.clj
+++ aerospike/project.clj
@@ -4,12 +4,19 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main aerospike.core
-  :dependencies [
-                 [org.clojure/clojure "1.11.1"]
-                 [jepsen "0.1.18"]
-                 [aerospike-client "7.2.0"]
-                 [clj-wallhack "1.0.1"]
-                ;;  [:local/root "/home/nlarsen/foo/jepsen/aerospike/JavaClientRepo/aerospike-client-7.2.0.jar"]
-                 ]
-  :repositories {"local" ~(str (.toURI (java.io.File. "/home/nlarsen/.local/var/lein_repo/aerospike-client/aerospike-client")))}
-)
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [jepsen "0.1.7"]
+                 [com.aerospike/aerospike-client "7.2.0"]
+                 [com.sun.xml.bind/jaxb-core "4.0.4"]
+                 [javax.xml.bind/jaxb-api "2.4.0-b180830.0359"]
+                 [com.sun.xml.bind/jaxb-impl "4.0.4"]
+                 [clj-wallhack "1.0.1"]])
+;;   :dependencies [
+;;                  [org.clojure/clojure "1.11.1"]
+;;                  [jepsen "0.1.18"]
+;;                  [aerospike-client "7.2.0"]
+;;                  [clj-wallhack "1.0.1"]
+;;                 ;;  [:local/root "/home/nlarsen/foo/jepsen/aerospike/JavaClientRepo/aerospike-client-7.2.0.jar"]
+;;                  ]
+;;   :repositories {"local" ~(str (.toURI (java.io.File. "/home/nlarsen/.local/var/lein_repo/aerospike-client/aerospike-client")))}
+;; )
