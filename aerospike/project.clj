8accf61487fa1eee1a4d38daefe39a5b1d9b5996
Aerospike-NateL <nlarsen@aerospike.com>
2023-12-18 11:08:38 -0800

    use local builds of jepsen

--- aerospike/project.clj
+++ aerospike/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main aerospike.core
   :dependencies [[org.clojure/clojure "1.12.0-alpha5"]
-                 [jepsen "0.3.4"]
+                 [jepsen "0.3.5a-aerospike"]
                  [com.aerospike/aerospike-client "7.2.0"]
                  [com.sun.xml.bind/jaxb-core "4.0.4"]
                  [javax.xml.bind/jaxb-api "2.4.0-b180830.0359"]
--- jepsen/project.clj
+++ jepsen/project.clj
@@ -1,4 +1,4 @@
-(defproject jepsen "0.3.5-SNAPSHOT"
+(defproject jepsen "0.3.5a-aerospike"
   :description "Distributed systems testing framework."
   :url         "https://jepsen.io"
   :license {:name "Eclipse Public License"
