a8d5375b7f562a58be31f1048da4bc2dff1e632e
Raunak Jalan <118164396+rjalan-yb@users.noreply.github.com>
2024-02-16 09:33:11 +0530

    YQ-1489 Upgrading Jepsen to 0.3.4 (#113)
    * YQ-1489 Upgrading clojure version to 0.3.5
    
    * YQ-1489 Upgrading clojure version to 0.3.5
    
    * YQ-1489 Upgrading clojure version to 0.3.5
    
    * YQ-1489 Upgrading clojure version to 0.3.5
    
    * YQ-1489 Upgrading clojure version to 0.3.4

--- yugabyte/project.clj
+++ yugabyte/project.clj
@@ -5,7 +5,7 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [clj-http "3.12.3" :exclusions [commons-logging]]
-                 [jepsen "0.3.3"]
+                 [jepsen "0.3.4"]
                  [com.yugabyte/cassaforte "3.0.0-alpha2-yb-1"]
                  [org.clojure/java.jdbc "0.7.12"]
                  [org.clojure/data.json "2.4.0"]
