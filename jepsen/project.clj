b7cd8036bf6d7a3ac3de518cca4baa702dc4c00c
Kyle Kingsbury <aphyr@aphyr.com>
2024-10-04 11:16:41 -0500

    Dependency updates
    Clojure 1.12.0
    tools.logging 1.3.0
    tools.cli 1.1.230
    unilog 0.7.32
    elle 0.2.2
    http-kit 2.8.0
    ring 1.12.2
    sshj 0.39.0
    data.codec 0.2.0

--- jepsen/project.clj
+++ jepsen/project.clj
@@ -3,22 +3,22 @@
   :url         "https://jepsen.io"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.clojure/data.fressian "1.0.0"]
-                 [org.clojure/tools.logging "1.2.4"]
-                 [org.clojure/tools.cli "1.0.219"]
-                 [spootnik/unilog "0.7.31"
+                 [org.clojure/tools.logging "1.3.0"]
+                 [org.clojure/tools.cli "1.1.230"]
+                 [spootnik/unilog "0.7.32"
                   :exclusions [org.slf4j/slf4j-api]]
-                 [elle "0.2.2-SNAPSHOT"]
+                 [elle "0.2.2"]
                  [clj-time "0.15.2"]
                  [io.jepsen/history "0.1.3"]
                  [jepsen.txn "0.1.2"]
                  [knossos "0.3.10"]
                  [clj-ssh "0.5.14"]
                  [gnuplot "0.1.3"]
-                 [http-kit "2.7.0"]
-                 [ring "1.11.0"]
-                 [com.hierynomus/sshj "0.38.0"]
+                 [http-kit "2.8.0"]
+                 [ring "1.12.2"]
+                 [com.hierynomus/sshj "0.39.0"]
                  [com.jcraft/jsch.agentproxy.connector-factory "0.0.9"]
                  [com.jcraft/jsch.agentproxy.sshj "0.0.9"
                   :exclusions [net.schmizz/sshj]]
@@ -27,7 +27,7 @@
                  [metametadata/multiset "0.1.1"]
                  [byte-streams "0.2.5-alpha2"]
                  [slingshot "0.12.2"]
-                 [org.clojure/data.codec "0.1.1"]
+                 [org.clojure/data.codec "0.2.0"]
                  [fipp "0.6.26"]]
   :java-source-paths ["src"]
   :javac-options ["--release" "11"]
