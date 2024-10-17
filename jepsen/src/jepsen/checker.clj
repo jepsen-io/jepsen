379b24122221964c0b3543bcc68b33b00f83d888
Kyle Kingsbury <aphyr@aphyr.com>
2024-10-17 11:31:45 -0500

    checker/check-safe: write errors to results.edn using datafy, not a horrible string.

--- jepsen/src/jepsen/checker.clj
+++ jepsen/src/jepsen/checker.clj
@@ -2,6 +2,7 @@
   "Validates that a history is correct with respect to some model."
   (:refer-clojure :exclude [set])
   (:require [clojure [core :as c]
+                     [datafy :refer [datafy]]
                      [pprint :refer [pprint]]
                      [set :as set]
                      [stacktrace :as trace]
@@ -79,7 +80,8 @@
 (defn check-safe
   "Like check, but wraps exceptions up and returns them as a map like
 
-  {:valid? :unknown :error \"...\"}"
+      {:valid? :unknown
+       :error {:via [{:type clojure.lang.Exceptioninfo, ...}] ...}"
   ([checker test history]
    (check-safe checker test history {}))
   ([checker test history opts]
@@ -87,7 +89,7 @@
         (catch Exception t
           (warn t "Error while checking history:")
           {:valid? :unknown
-           :error (with-out-str (trace/print-cause-trace t))}))))
+           :error (datafy t)}))))
 
 (defn compose
   "Takes a map of names to checkers, and returns a checker which runs each
