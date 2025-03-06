c2c9c2df28d165248759fcb81a5e2a112f596e1c
Kyle Kingsbury <aphyr@aphyr.com>
2025-03-06 17:12:51 -0600

    Version 0.3.8

--- jepsen/project.clj
+++ jepsen/project.clj
@@ -1,4 +1,4 @@
-(defproject jepsen "0.3.8-SNAPSHOT"
+(defproject jepsen "0.3.8"
   :description "Distributed systems testing framework."
   :url         "https://jepsen.io"
   :license {:name "Eclipse Public License"
