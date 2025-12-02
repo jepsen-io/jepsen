bbdb47a4c745e3f781b41f1b93d915e296e8fb61
Kyle Kingsbury <aphyr@aphyr.com>
2025-12-02 17:02:52 -0600

    Extract CockroachDB to its own repo

--- cockroachdb/full.sh
+++
@@ -1,9 +0,0 @@
-lein do clean, run test \
-  --tarball http://aphyr.com/media/cockroach-2017-03-30.tar.bz2 \
-  --username admin \
-  --nodes-file ~/nodes \
-  --test sets \
-  --nemesis majority-ring \
-  --nemesis2 subcritical-skews \
-  --time-limit 180 \
-  --test-count 10
