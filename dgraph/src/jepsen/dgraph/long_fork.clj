827c6eeaaabedf86c9d8d62c1b00883a3d087ecb
Kyle Kingsbury <aphyr@aphyr.com>
2025-12-02 17:04:13 -0600

    Extract DGraph to its own repo

--- dgraph/src/jepsen/dgraph/long_fork.clj
+++
@@ -1,8 +0,0 @@
-(ns jepsen.dgraph.long-fork
-  (:require [jepsen.tests.long-fork :as lf]
-            [jepsen.dgraph.client :as c]))
-
-(defn workload
-  [opts]
-  (assoc (lf/workload 3)
-         :client (c/txn-client {:blind-insert-on-write? true})))
