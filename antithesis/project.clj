f9b56ee5857b577cbcfebc5d1605c362328d6283
Kyle Kingsbury <aphyr@aphyr.com>
2026-03-16 22:14:47 -0500

    Antithesis: version 0.1.0

--- antithesis/project.clj
+++ antithesis/project.clj
@@ -1,9 +1,9 @@
-(defproject io.jepsen/antithesis "0.1.0-SNAPSHOT"
+(defproject io.jepsen/antithesis "0.1.0"
   :description "Support for running Jepsen inside Antithesis"
   :url "https://github.com/jepsen-io/jepsen"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[jepsen "0.3.11-SNAPSHOT"
+  :dependencies [[jepsen "0.3.11"
                   ; Jepsen pulls in jackson 2.16, but the Antithesis SDK
                   ; expects 2.2.3
                   :exclusions [com.fasterxml.jackson.core/jackson-databind
