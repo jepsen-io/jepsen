f06cd844027475c03f7191e708c780ecf573a88b
Kyle Kingsbury <aphyr@aphyr.com>
2026-03-16 22:16:48 -0500

    antithesis: 0.1.1-SNAPSHOT

--- antithesis/project.clj
+++ antithesis/project.clj
@@ -1,4 +1,4 @@
-(defproject io.jepsen/antithesis "0.1.0"
+(defproject io.jepsen/antithesis "0.1.1-SNAPSHOT"
   :description "Support for running Jepsen inside Antithesis"
   :url "https://github.com/jepsen-io/jepsen"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
