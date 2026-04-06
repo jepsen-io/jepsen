9858d8ced3f71e0e127d3dac3e1b1727f341b8f5
Jeff Suttor <jeff.suttor@earthlink.net>
2026-04-05 22:18:00 -0700

    expand jepsen.lazyfs-test to test fsynced or unfsynced writes using an interleaved or phased generator

--- jepsen/test/jepsen/lazyfs_test.clj
+++ jepsen/test/jepsen/lazyfs_test.clj
@@ -1,95 +1,270 @@
 (ns jepsen.lazyfs-test
-  "Tests for the lazyfs write-losing filesystem"
-  (:require [clojure [data :refer [diff]]
-                     [pprint :refer [pprint]]
-                     [string :as str]
-                     [test :refer :all]]
-            [clojure.java.io :as io]
-            [clojure.test.check [clojure-test :refer :all]
-                                [generators :as g]
-                                [properties :as prop]
-                                [results :refer [Result]]]
-            [clojure.tools.logging :refer [info warn]]
-            [dom-top.core :refer [loopr]]
-            [jepsen [checker :as checker]
-                    [client :as client]
-                    [common-test :refer [quiet-logging]]
-                    [control :as c]
-                    [core :as jepsen]
-                    [db :as db]
-                    [generator :as gen]
-                    [lazyfs :as lazyfs]
-                    [nemesis :as nem]
-                    [os :as os]
-                    [tests :as tests]
-                    [util :as util :refer [pprint-str
-                                           timeout]]]
-            [jepsen.control [core :as cc]
-                            [util :as cu]
-                            [sshj :as sshj]]
-            [jepsen.os.debian :as debian]
-            [clj-commons.slingshot :refer [try+ throw+]]))
+  "Tests the `lazyfs` filesystem.
+   
+   Tries losing both fsynced and unfsynced writes.
+   
+   Tests using two styles of generators:
 
-; (use-fixtures :once quiet-logging)
+   - interleaved Client+Nemesis generator that:
+       - interleaves Client writes, may be fsynced or unfsynced,
+         and Nemesis losing unfsynced writes
+       - followed by a final read
+   - phased generator that:
+       - does unfsynced writes
+       - an optional fsync
+       - loses unfsynced writes
+       - followed by a final read
+   
+   A fsynced Client write is two individual commands:
+   
+   ```clj
+   (c/exec :echo (str value \" \") :>> file)
+   (c/exec :sync file)
+   ```
 
-(defrecord FileSetClient [dir file node]
-  client/Client
-  (open! [this test node]
-    (assoc this
-           :file (str dir "/set")
-           :node node))
+   When using a interleaved Client+Nemesis generator with synced writes,
+   the [[FileSetClient]] and [[LazyfsNemesis]] need to coordinate to avoid the
+   Nemesis' [[jepsen.lazyfs/lose-unfsynced-writes!]] occurring between the Client's
+   write and fsync. This is done with `(locking fsync-lock)`.
+   
+   There is no `locking` with unfsynced writes or phased generators."
+  (:require
+   [clojure
+    [string :as str]
+    [test :refer [deftest is]]]
+   [jepsen [checker :as checker]
+    [client :as client]
+    [control :as c]
+    [core :as jepsen]
+    [generator :as gen]
+    [lazyfs :as lazyfs]
+    [nemesis :as nemesis]
+    [tests :as tests]
+    [util :as u]]
+   [jepsen.os.debian :as debian]))
 
-  (setup! [this test]
+(def dir
+  "Directory to store set file."
+  "/tmp/jepsen/lazyfs-test")
+
+(def node
+  "Node to run test on."
+  "n1")
+
+(defn write-ops
+  "Given a `fsync-writes?` flag, returns a lazy sequence of write ops.
+   Ops may contain `{:fsync? true}`."
+  [fsync-writes?]
+  (->> (range)
+       (map (fn [value]
+              (merge
+               {:type  :invoke
+                :f     :add
+                :value value}
+               (when fsync-writes?
+                 {:fsync? true}))))))
+
+(defn read-op
+  "Given a `final-read?` flag, returns a read op.
+   Ops may contain `{final? true}`."
+  [final-read?]
+  (merge
+   {:type  :invoke
+    :f     :read
+    :value nil}
+   (when final-read?
+     {:final? true})))
+
+(def fsync-op
+  "An fsync op."
+  {:type  :invoke
+   :f     :fsync
+   :value nil})
+
+(def lose-unfsynced-writes-op
+  "A `lose-unfsynced-writes` op for [[node]]."
+  {:type  :info
+   :f     :lose-unfsynced-writes
+   :value [node]})
+
+(def fsync-lock
+  "Object to coordinate Client's write/fsync and Nemesis' `lose-unfsynced-writes`."
+  (Object.))
+
+; A copy of [[jepsen.lazyfs/nemesis]].
+; If fsync?, will `(locking fsync-lock)` to not interleave with Client's write and fsync commands."
+(defrecord LazyfsNemesis [lazyfs fsync?]
+  nemesis/Nemesis
+  (setup! [this _test]
     this)
 
-  (invoke! [this test op]
-    (-> (c/on-nodes test [node]
-                    (fn [_ _]
-                      (case (:f op)
-                        :add  (do (c/exec :echo (str (:value op) " ") :>> file)
-                                  (assoc op :type :ok))
-                        :read (let [vals (-> (c/exec :cat file)
-                                             (str/split #"\s+")
-                                             (->> (remove #{""})
-                                                  (mapv parse-long)))]
-                                (assoc op :type :ok, :value vals)))))
-        (get node)))
+  (invoke! [_this test {:keys [f] :as op}]
+    (case f
+      :lose-unfsynced-writes
+      (let [v (c/on-nodes test (:value op)
+                          (fn [_test _node]
+                            (if-not fsync?
+                              (lazyfs/lose-unfsynced-writes! lazyfs)
+                              (locking fsync-lock
+                                (lazyfs/lose-unfsynced-writes! lazyfs)))))]
+        (assoc op :value v))))
 
-  (teardown! [this test])
+  (teardown! [_this _test])
 
-  (close! [this test]))
+  nemesis/Reflection
+  (fs [_this]
+    #{:lose-unfsynced-writes}))
 
-(defn file-set-client
-  "Writes a set to a single file on one node, in the given directory."
-  [dir]
-  (map->FileSetClient {:dir dir}))
+; The `FileSetClient` will fsync writes if the `op` contains `{:fsync? true}`.
+(defrecord FileSetClient []
+  client/Client
+  (open! [this _test _node]
+    (assoc this
+           :file (str dir "/" (u/rand-distribution) ".set")))
 
-(deftest ^:integration file-set-test
-  (let [dir    "/tmp/jepsen/file-set-test"
-        lazyfs (lazyfs/lazyfs dir)
-        test (assoc tests/noop-test
-                    :name      "lazyfs file set"
-                    :os        debian/os
-                    :db        (lazyfs/db lazyfs)
-                    :client    (file-set-client dir)
-                    :nemesis   (lazyfs/nemesis lazyfs)
-                    :generator (gen/phases
-                                 (->> (range)
-                                      (map (fn [x] {:f :add, :value x}))
-                                      (gen/delay 1/100)
-                                      (gen/nemesis
-                                        (->> {:type :info
-                                              :f    :lose-unfsynced-writes
-                                              :value ["n1"]}
-                                             repeat
-                                             (gen/delay 1/2)))
-                                      (gen/time-limit 5))
-                                 (gen/clients {:f :read}))
-                    :checker   (checker/set)
-                    :nodes     ["n1"])
-        test (jepsen/run! test)]
-    ;(pprint (:history test))
-    ;(pprint (:results test))
-    (is (false? (:valid? (:results test))))
-    (is (pos? (:ok-count (:results test))))
-    (is (pos? (:lost-count (:results test))))))
+  (setup! [{:keys [file] :as _this} test]
+    ; insure file doesn't exist, i.e. don't reuse
+    (c/with-node test node
+      (c/su
+       (c/exec :rm :-rf file))))
+
+  (invoke! [{:keys [file] :as _this} test {:keys [f fsync? value] :as op}]
+    (c/with-node test node
+      (case f
+        :add  (do
+                (if-not fsync?
+                  (c/exec :echo (str value " ") :>> file)
+                  (locking fsync-lock
+                    (c/exec :echo (str value " ") :>> file)
+                    (c/exec :sync file)))
+                (assoc op :type :ok))
+
+        :read (let [vals (-> (c/exec :cat file)
+                             (str/split #"\s+")
+                             (->> (remove #{""})
+                                  (mapv parse-long)))]
+                (assoc op :type :ok, :value vals))
+
+        :fsync (do
+                 (c/exec :sync file)
+                 (assoc op :type :ok)))))
+
+  (teardown! [_this _test])
+
+  (close! [_this _test]))
+
+(defn interleaved-gen
+  "Given a `fsync?` flag,
+   returns a generator that:
+
+   - interleaves 
+       - Client writes, `fsync?` flag determines fsyncing 
+       - Nemesis losing unfsynced writes
+   - for 5 seconds
+   - then a final read"
+  [fsync?]
+  (gen/phases
+   (gen/log (str "Combining Client writes (fsync? " fsync? "), and Nemesis losing unfsynced writes..."))
+   (->> (->> (write-ops fsync?)
+             (gen/delay 1/100))
+        (gen/nemesis
+         (->> lose-unfsynced-writes-op
+              repeat
+              (gen/delay 1/2)))
+        (gen/time-limit 5))
+
+   (gen/log "Final reads...")
+   (gen/clients (read-op true))))
+
+(defn phased-gen
+  "Given a `fsync?` flag,
+   returns a generator that does in phases:
+
+   - 100 writes, unfsynced
+   - optionally fsyncs
+   - loses unfsynced writes
+   - final read"
+  [fsync?]
+  (gen/phases
+   (gen/log "100 writes (unfsynced)...")
+   (->> (write-ops false)
+        (take 100)
+        (gen/delay 1/100)
+        (gen/clients))
+
+   (when fsync?
+     (gen/log "fsyncing")
+     (gen/clients fsync-op))
+
+   (gen/log "losing unfsynced writes")
+   (gen/nemesis lose-unfsynced-writes-op)
+
+   (gen/log "final reads...")
+   (gen/clients (read-op true))))
+
+(defn test-map
+  "Given a `type`, one of `#{:interleaved :phased}`, and a `fsync?` flag,
+   returns a test map that writes/read to a unique file in [[dir]],
+   using `type` of generator and optionally fsyncing writes."
+  [type fsync?]
+  (assert (#{:interleaved :phased} type))
+  (let [lazyfs (lazyfs/lazyfs dir)
+        [generator lazyfs-nemesis]
+        (case [type fsync?]
+          [:interleaved false] [(interleaved-gen false) (LazyfsNemesis. lazyfs false)]
+          [:interleaved true]  [(interleaved-gen true)  (LazyfsNemesis. lazyfs true)]
+          [:phased      false] [(phased-gen      false) (LazyfsNemesis. lazyfs false)]
+          [:phased      true]  [(phased-gen      true)  (LazyfsNemesis. lazyfs false)])]
+    (assoc tests/noop-test
+           :name      "lazyfs file set"
+           :os        debian/os
+           :db        (lazyfs/db lazyfs)
+           :client    (FileSetClient.)
+           :nemesis   lazyfs-nemesis
+           :generator generator
+           :checker   (checker/set)
+           :nodes     [node])))
+
+(deftest ^:integration lazyfs-test
+  (let [interleaved-unfsynced-map  (test-map :interleaved false)
+        interleaved-unfsynced-test (jepsen/run! interleaved-unfsynced-map)
+
+        interleaved-synced-map  (test-map :interleaved true)
+        interleaved-synced-test (jepsen/run! interleaved-synced-map)
+
+        phased-unfsynced-map  (test-map :phased false)
+        phased-unfsynced-test (jepsen/run! phased-unfsynced-map)
+
+        phased-synced-map  (test-map :phased true)
+        phased-synced-test (jepsen/run! phased-synced-map)]
+
+    ; unfsynced writes, interleaved generator 
+    (let [{:keys [acknowledged-count attempt-count ok-count lost-count recovered-count unexpected-count valid?]}
+          (-> interleaved-unfsynced-test :results)]
+      (is (not valid?))
+      (is (pos? acknowledged-count))
+      (is (< ok-count attempt-count))
+      (is (pos? ok-count))
+      (is (pos? lost-count))
+      (is (= 0 recovered-count unexpected-count)))
+
+    ; unfsynced writes, phased generator
+    (let [{:keys [acknowledged-count attempt-count ok-count lost-count recovered-count unexpected-count valid?]}
+          (-> phased-unfsynced-test :results)]
+      (is (not valid?))
+      (is (= acknowledged-count attempt-count lost-count))
+      (is (= 0 ok-count recovered-count unexpected-count)))
+
+    ; fsynced writes, interleaved generator
+    (let [{:keys [acknowledged-count attempt-count ok-count lost-count recovered-count unexpected-count valid?]}
+          (-> interleaved-synced-test :results)]
+      (is valid?)
+      (is (= acknowledged-count attempt-count ok-count))
+      (is (= 0 lost-count recovered-count unexpected-count)))
+
+    ; unfsynced writes, phased generator
+    (let [{:keys [acknowledged-count attempt-count ok-count lost-count recovered-count unexpected-count valid?]}
+          (-> phased-synced-test :results)]
+      (is valid?)
+      (is (= acknowledged-count attempt-count ok-count))
+      (is (= 0 lost-count recovered-count unexpected-count)))))
