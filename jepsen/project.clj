3d07ea7e9a6ffcf46af9d509bfd4b87f3805963c
Kyle Kingsbury <aphyr@aphyr.com>
2026-07-20 12:47:28 -0500

    0.3.13-SNAPSHOT

--- jepsen/project.clj
+++ jepsen/project.clj
@@ -1,4 +1,4 @@
-(defproject jepsen "0.3.12"
+(defproject jepsen "0.3.13-SNAPSHOT"
   :description "Distributed systems testing framework."
   :url         "https://jepsen.io"
   :scm     {:name "git"
